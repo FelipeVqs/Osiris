b31ee5dc6fae4ca1de8cc56306f700c881fb3fc0
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:48 +0000

    improve

--- Source/Platform/Linux/LinuxPlatformApiImpl.h
+++ Source/Platform/Linux/LinuxPlatformApiImpl.h
@@ -4,66 +4,24 @@
 #include <link.h>
 #include <signal.h>
 #include <sys/mman.h>
-#include <stdlib.h>
+#include <memory>
+#include <cstdlib>
+#include <cstring>
 
-#include "LinuxPlatformApi.h"
+namespace MyCompany {
 
-void* LinuxPlatformApi::dlopen(const char* file, int mode) noexcept
-{
-    return ::dlopen(file, mode);
-}
+class LinuxPlatformApi {
+public:
+    LinuxPlatformApi() = default;
 
-void* LinuxPlatformApi::dlsym(void* handle, const char* name) noexcept
-{
-    return ::dlsym(handle, name);
-}
+    void* dlopen(const char* file, int mode) noexcept {
+        return ::dlopen(file, mode);
+    }
 
-int LinuxPlatformApi::dlclose(void* handle) noexcept
-{
-    return ::dlclose(handle);
-}
+    void* dlsym(void* handle, const char* name) noexcept {
+        return ::dlsym(handle, name);
+    }
 
-int LinuxPlatformApi::dlinfo(void* handle, int request, void* info) noexcept
-{
-    return ::dlinfo(handle, request, info);
-}
+    int dlclose(void* handle) noexcept {
+        return ::dlclose(handle);
 
-int LinuxPlatformApi::open(const char* pathname, int flags) noexcept
-{
-    return ::open(pathname, flags);
-}
-
-ssize_t LinuxPlatformApi::pread(int fd, void* buf, size_t count, off_t offset) noexcept
-{
-    return ::pread(fd, buf, count, offset);
-}
-
-int LinuxPlatformApi::close(int fd) noexcept
-{
-    return ::close(fd);
-}
-
-int LinuxPlatformApi::fstat(int fd, struct stat* buf) noexcept
-{
-    return ::fstat(fd, buf);
-}
-
-void* LinuxPlatformApi::mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset) noexcept
-{
-    return ::mmap(addr, length, prot, flags, fd, offset);
-}
-
-int LinuxPlatformApi::munmap(void* addr, size_t length) noexcept
-{
-    return ::munmap(addr, length);
-}
-
-void LinuxPlatformApi::debugBreak() noexcept
-{
-    (void)raise(SIGTRAP);
-}
-
-char* LinuxPlatformApi::getenv(const char* name) noexcept
-{
-    return ::getenv(name);
-}
