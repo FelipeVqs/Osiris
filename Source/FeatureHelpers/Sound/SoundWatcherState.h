c6d6961a8abd33ff0fffd92cd9f88efa17fb6198
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:27 +0000

    improve

--- Source/FeatureHelpers/Sound/SoundWatcherState.h
+++ Source/FeatureHelpers/Sound/SoundWatcherState.h
@@ -1,17 +1,10 @@
-#include <Features/Sound/Details/BombBeepSound.h>
-#include <Features/Sound/Details/BombDefuseSound.h>
-#include <Features/Sound/Details/BombPlantSound.h>
-#include <Features/Sound/Details/FootstepSound.h>
-#include <Features/Sound/Details/WeaponReloadSound.h>
-#include <Features/Sound/Details/WeaponScopeSound.h>
+#include <Sound/SoundDetails.h> // assuming this includes all the necessary sound headers
 
-#include "SoundWatcherImplState.h"
-
+// A state type that includes all the necessary sound types.
 using SoundWatcherState = SoundWatcherImplState<
     FootstepSound,
     BombPlantSound,
     BombBeepSound,
     BombDefuseSound,
     WeaponScopeSound,
-    WeaponReloadSound
->;
+
