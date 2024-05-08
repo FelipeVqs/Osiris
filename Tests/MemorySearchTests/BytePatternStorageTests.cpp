22d02ef9ceb17b5bd3e028ac19288368906870e6
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:39 +0000

    improve

--- Tests/MemorySearchTests/BytePatternStorageTests.cpp
+++ Tests/MemorySearchTests/BytePatternStorageTests.cpp
@@ -1,24 +1,41 @@
-#include <string_view>
+#ifndef BYTE_PATTERN_STORAGE_TEST_H
+#define BYTE_PATTERN_STORAGE_TEST_H
 
 #include <gtest/gtest.h>
-
 #include <MemorySearch/BytePatternStorage.h>
 
 namespace
 {
+using BytePattern = MemorySearch::BytePatternStorage;
+using BytePatternView = std::string_view;
 
-using namespace std::string_view_literals;
+BytePatternView asBytePatternView(const BytePattern& pattern)
+{
+    return BytePatternView{ pattern.pattern.data(), pattern.size };
+}
 
-TEST(BytePatternStorageTest, StorageHasSizeEqualToLengthOfConvertedPattern) {
-    constexpr BytePatternStorage storage{ "83 EC ? ? 4C ? 80 3D" };
+}  // namespace
+
+TEST(BytePatternStorageTest, StorageHasSizeEqualToLengthOfConvertedPattern)
+{
+    constexpr BytePattern storage{ "83 EC ? ? 4C ? 80 3D" };
     EXPECT_EQ(storage.size, 8);
 }
 
-TEST(BytePatternStorageTest, PatternConversionIsPerformedInConstructor) {
-    constexpr BytePatternStorage storage{ "E8 ? ? ? ? 48 00 C0 74 62" };
-    constexpr auto expectedPattern = "\xE8????\x48\x00\xC0\x74\x62"sv;
-    const auto actualPattern = std::string_view{ storage.pattern.data(), storage.size };
+TEST(BytePatternStorageTest, PatternConversionIsPerformedInConstructor)
+{
+    constexpr BytePattern storage{ "E8 ? ? ? ? 48 00 C0 74 62" };
+    constexpr BytePatternView expectedPattern = "\xE8????\x48\x00\xC0\x74\x62"sv;
+    const auto actualPattern = asBytePatternView(storage);
     EXPECT_EQ(actualPattern, expectedPattern);
 }
 
+TEST(BytePatternStorageTest, PatternConversionCorrectlyEscapesBackslashes)
+{
+    constexpr BytePattern storage{ "E8 \\? \\? \\? \\? 48 00 C0 74 62" };
+    constexpr BytePatternView expectedPattern = "\xE8\\?\\?\\?\\?\x48\x00\xC0\x74\x62"sv;
+    const auto actualPattern = asBytePatternView(storage);
+    EXPECT_EQ(actualPattern, expectedPattern);
 }
+
+#endif // BYTE_PATTERN_STORAGE_TEST_H
