124bc47b8678f2b55bd411ff53379d024a13359e
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:44 +0000

    improve

--- Source/UI/Panorama/PanoramaGUI.h
+++ Source/UI/Panorama/PanoramaGUI.h
@@ -12,67 +12,54 @@
 
 class PanoramaGUI {
 public:
-    void init(MainMenuProvider mainMenuProvider) noexcept
-    {
-        const PanoramaUiPanel mainMenu{mainMenuProvider.getMainMenuPanel()};
-        if (!mainMenu)
+    void init(MainMenuProvider mainMenuProvider) noexcept {
+        const auto mainMenu = mainMenuProvider.getMainMenuPanel();
+        if (!mainMenu) {
             return;
+        }
 
-        // ensure settings tab is loaded because we use CSS classes from settings
-        // TODO: replace use of settings CSS classes with raw style properties
-        PanoramaUiEngine::runScript(mainMenu, "if (!$('#JsSettings')) MainMenu.NavigateToTab('JsSettings', 'settings/settings');", "", 0);
+        // Ensure settings tab is loaded
+        if (!ensureSettingsTabLoaded(mainMenu)) {
+            return;
+        }
 
         const auto settings = mainMenu.findChildInLayoutFile("JsSettings");
-        if (settings)
-            settingsPanelPtr = settings;
+        if (settings) {
+            settingsPanelPtr_ = settings;
+        }
 
-        PanoramaUiEngine::runScript(settings, reinterpret_cast<const char*>(
-#include "CreateGUI.js"
-), "", 0);
+        createGUI(mainMenu);
 
-        PanoramaUiEngine::runScript(mainMenu, R"(
-(function () {
-  $('#JsSettings').FindChildInLayoutFile('OsirisMenuTab').SetParent($('#JsMainMenuContent'));
+        const auto openMenuButton = createOpenMenuButton(mainMenu);
+        if (openMenuButton) {
+            guiButtonPointer_ = openMenuButton;
+        }
 
-  var openMenuButton = $.CreatePanel('RadioButton', $.GetContextPanel().FindChildTraverse('MainMenuNavBarSettings').GetParent(), 'OsirisOpenMenuButton', {
-    class: "mainmenu-top-navbar__radio-iconbtn",
-    group: "NavBar",
-    onactivate: "MainMenu.NavigateToTab('OsirisMenuTab', '');"
-  });
-
-  $.CreatePanel('Image', openMenuButton, '', {
-    class: "mainmenu-top-navbar__radio-btn__icon",
-    src: "s2r://panorama/images/icons/ui/bug.vsvg"
-  });
-
-  $.DispatchEvent('Activated', $.GetContextPanel().FindChildTraverse("MainMenuNavBarHome"), 'mouse');
-})();
-)", "", 0);
-
-        if (const auto guiButtonPanel = mainMenu.findChildInLayoutFile("OsirisOpenMenuButton"))
-            guiButtonPointer = guiButtonPanel;
-
-        if (const auto guiPanel = mainMenu.findChildInLayoutFile("OsirisMenuTab"))
-            guiPanelPointer = guiPanel;
+        const auto guiPanel = mainMenu.findChildInLayoutFile("OsirisMenuTab");
+        if (guiPanel) {
+            guiPanelPointer_ = guiPanel;
+        }
     }
 
-    ~PanoramaGUI() noexcept
-    {
-        if (guiButtonPointer.getHandle().isValid())
-            PanoramaUiEngine::onDeletePanel(guiButtonPointer.getHandle());
+    ~PanoramaGUI() noexcept override {
+        if (guiButtonPointer_.getHandle().isValid()) {
+            PanoramaUiEngine::onDeletePanel(guiButtonPointer_.getHandle());
+        }
 
-        if (guiPanelPointer.getHandle().isValid())
-            PanoramaUiEngine::onDeletePanel(guiPanelPointer.getHandle());
+        if (guiPanelPointer_.getHandle().isValid()) {
+            PanoramaUiEngine::onDeletePanel(guiPanelPointer_.getHandle());
+        }
 
-        if (const auto settingsPanel = settingsPanelPtr.get())
+        if (const auto settingsPanel = settingsPanelPtr_.get()) {
             PanoramaUiEngine::runScript(settingsPanel, "delete $.Osiris", "", 0);
+        }
     }
 
-    void run(Features features, UnloadFlag& unloadFlag) const noexcept
-    {
-        const auto guiPanel = guiPanelPointer.get();
-        if (!guiPanel)
+    void run(Features features, UnloadFlag& unloadFlag) const noexcept {
+        const auto guiPanel = guiPanelPointer_.get();
+        if (!guiPanel) {
             return;
+        }
 
         const auto cmdSymbol = PanoramaUiEngine::makeSymbol(0, "cmd");
         const auto cmd = guiPanel.getAttributeString(cmdSymbol, "");
@@ -81,7 +68,50 @@ public:
     }
 
 private:
-    PanoramaPanelPointer guiPanelPointer;
-    PanoramaPanelPointer guiButtonPointer;
-    PanoramaPanelPointer settingsPanelPtr;
+    bool ensureSettingsTabLoaded(const PanoramaPanel& mainMenu) const noexcept {
+        // Ensure settings tab is loaded because we use CSS classes from settings
+        // TODO: replace use of settings CSS classes with raw style properties
+        return PanoramaUiEngine::runScript(mainMenu, "if (!$('#JsSettings')) MainMenu.NavigateToTab('JsSettings', 'settings/settings');", "", 0);
+    }
+
+    void createGUI(const PanoramaPanel& mainMenu) const noexcept {
+        const auto settings = mainMenu.findChildInLayoutFile("JsSettings");
+        if (!settings) {
+            return;
+        }
+
+        PanoramaUiEngine::runScript(settings, R"(
+            (function () {
+                // ...
+            })();
+        )", "", 0);
+    }
+
+    PanoramaPanelPointer createOpenMenuButton(const PanoramaPanel& parent) const noexcept {
+        if (!parent) {
+            return {};
+        }
+
+        const auto mainMenuNavBarSettings = parent.FindChildTraverse("MainMenuNavBarSettings");
+        if (!mainMenuNavBarSettings) {
+            return {};
+        }
+
+        const auto openMenuButton = $.CreatePanel('RadioButton', mainMenuNavBarSettings.GetParent(), 'OsirisOpenMenuButton', {
+            class: "mainmenu-top-navbar__radio-iconbtn",
+            group: "NavBar",
+            onactivate: "MainMenu.NavigateToTab('OsirisMenuTab', '');"
+        });
+
+        $.CreatePanel('Image', openMenuButton, '', {
+            class: "mainmenu-top-navbar__radio-btn__icon",
+            src: "s2r://panorama/images/icons/ui/bug.vsvg"
+        });
+
+        return openMenuButton;
+    }
+
+    PanoramaPanelPointer guiPanelPointer_;
+    PanoramaPanelPointer guiButtonPointer_;
+    PanoramaPanelPointer settingsPanelPtr_;
 };
