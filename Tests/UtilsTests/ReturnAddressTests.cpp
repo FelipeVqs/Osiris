58c930e3bfd7c54cfe6e6dc5f8ae679cbe26965a
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:52 +0000

    improve

--- Tests/UtilsTests/ReturnAddressTests.cpp
+++ Tests/UtilsTests/ReturnAddressTests.cpp
@@ -7,7 +7,9 @@
 namespace
 {
 
-class ReturnAddressTest : public testing::TestWithParam<std::pair<void*, void*>> {};
+using PairType = std::pair<void*, void*>;
+
+class ReturnAddressTest : public testing::TestWithParam<PairType> {};
 
 TEST_P(ReturnAddressTest, SameReturnAddressesCompareEqual) {
     EXPECT_EQ(ReturnAddress{GetParam().first}, ReturnAddress{GetParam().first});
@@ -18,10 +20,11 @@ TEST_P(ReturnAddressTest, DifferentReturnAddressesDoNotCompareEqual) {
     EXPECT_NE(ReturnAddress{GetParam().second}, ReturnAddress{GetParam().first});
 }
 
-INSTANTIATE_TEST_SUITE_P(, ReturnAddressTest, testing::Values(
-    std::pair{nullptr, reinterpret_cast<void*>(1234)},
-    std::pair{reinterpret_cast<void*>(2048), reinterpret_cast<void*>(-1)},
-    std::pair{reinterpret_cast<void*>(-1), nullptr}
-));
+INSTANTIATE_TEST_SUITE_P(ReturnAddressTestSuite, ReturnAddressTest, testing::ValuesIn(std::initializer_list<PairType>{
+    {nullptr, reinterpret_cast<void*>(1234)},
+    {reinterpret_cast<void*>(2048), reinterpret_cast<void*>(-1)},
+    {reinterpret_cast<void*>(-1), nullptr},
+    {nullptr, nullptr}
+}));
 
 }
