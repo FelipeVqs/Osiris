22f5b7156cda8e8d0432389c0463a033322557cc
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:37 +0000

    improve

--- Tests/Platform/Windows/PebLdrTests.cpp
+++ Tests/Platform/Windows/PebLdrTests.cpp
@@ -9,60 +9,30 @@ namespace
 
 class TestPebLdr : public testing::Test {
 protected:
-    void addDummyModule1()
+    void addDummyModule(void* handle, const std::string& dllName)
     {
-        builder.addModule((void*)0x1234123412341234, L"dllone");
-    }
-
-    void addDummyModule2()
-    {
-        builder.addModule((void*)0x0BADC0DE0BADC0DE, L"another.dll");
+        builder.addModule(handle, std::wstring(dllName.begin(), dllName.end()).c_str());
     }
 
     PebLdrBuilder builder;
 };
 
 TEST_F(TestPebLdr, NullHandleIsReturnedWhenModuleIsNotFound) {
-    addDummyModule1();
-    addDummyModule2();
+    addDummyModule((void*)0x1234123412341234, "dllone");
+    addDummyModule((void*)0x0BADC0DE0BADC0DE, "another.dll");
     EXPECT_EQ(builder.pebLdr().getModuleHandle("client.dll"), nullptr);
 }
 
-class TestPebLdr_Handle_Name
-    : public TestPebLdr, public testing::WithParamInterface<std::tuple<void*, std::string>> {};
+class TestPebLdr_Handle_Name : public TestPebLdr, public testing::WithParamInterface<std::tuple<void*, std::string>> {};
 
-TEST_P(TestPebLdr_Handle_Name, FirstModuleInListCanBeFound) {
+TEST_P(TestPebLdr_Handle_Name, ModuleCanBeFound) {
     const auto [handle, dllName] = GetParam();
-    const std::wstring dllNameUnicode{ dllName.begin(), dllName.end() };
-    builder.addModule(handle, dllNameUnicode.c_str());
 
-    addDummyModule1();
-    addDummyModule2();
+    addDummyModule((void*)0xABABABABABABABAB, "dummy1.dll");
+    addDummyModule((void*)0xCDCDCDCDCDCDCDCD, "dummy2.dll");
+    addDummyModule(handle, dllName);
 
-    EXPECT_EQ(builder.pebLdr().getModuleHandle(dllName.c_str()), handle);
-}
-
-TEST_P(TestPebLdr_Handle_Name, LastModuleInListCanBeFound) {
-    addDummyModule1();
-    addDummyModule2();
-
-    const auto [handle, dllName] = GetParam();
-    const std::wstring dllNameUnicode{ dllName.begin(), dllName.end() };
-    builder.addModule(handle, dllNameUnicode.c_str());
-
-    EXPECT_EQ(builder.pebLdr().getModuleHandle(dllName.c_str()), handle);
-}
-
-TEST_P(TestPebLdr_Handle_Name, ModuleInTheCenterOfListCanBeFound) {
-    addDummyModule1();
-
-    const auto [handle, dllName] = GetParam();
-    const std::wstring dllNameUnicode{ dllName.begin(), dllName.end() };
-    builder.addModule(handle, dllNameUnicode.c_str());
-
-    addDummyModule2();
-
-    EXPECT_EQ(builder.pebLdr().getModuleHandle(dllName.c_str()), handle);
+    EXPECT_EQ(builder.pebLdr().getModuleHandle(dllName), handle);
 }
 
 INSTANTIATE_TEST_SUITE_P(, TestPebLdr_Handle_Name, testing::Combine(
