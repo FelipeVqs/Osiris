478ea3d86e7a10fe36be0282b2ca363869dfef4f
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:49 +0000

    improve

--- Source/CS2/Classes/Panorama.h
+++ Source/CS2/Classes/Panorama.h
@@ -1,91 +1,88 @@
-#pragma once
+#ifndef CS2_PANORAMA_H
+#define CS2_PANORAMA_H
 
 #include <cstdint>
+#include <string>
+#include <vector>
 
-#include <Platform/Macros/IsPlatform.h>
-#include <Platform/Macros/PlatformSpecific.h>
 #include <Utils/Pad.h>
 
 #include "Color.h"
-#include "CUtlMap.h"
-#include "CUtlString.h"
-#include "CUtlVector.h"
 #include "Vector.h"
 
-namespace cs2
-{
+namespace cs2 {
 
-using CPanoramaSymbol = short;
-
-struct PanelHandle {
-    std::int32_t panelIndex = -1;
-    std::uint32_t serialNumber;
-
-    [[nodiscard]] bool isValid() const noexcept
-    {
-        return panelIndex != -1;
-    }
-
-    [[nodiscard]] constexpr bool operator==(const PanelHandle&) const = default;
-};
-
-struct CUIPanel;
-
-struct CUIEngine {
-#if IS_WIN64()
-    using getPanelHandle = void (*)(CUIEngine* thisptr, PanelHandle* handle, CUIPanel* panel);
-#elif IS_LINUX()
-    using getPanelHandle = PanelHandle (*)(CUIEngine* thisptr, CUIPanel* panel);
-#endif
-    using getPanelPointer = CUIPanel* (*)(CUIEngine* thisptr, const PanelHandle* handle);
-    using runScript = void (*)(CUIEngine* thisptr, CUIPanel* contextPanel, const char* scriptSource, const char* originFile, std::uint64_t line);
-    using makeSymbol = CPanoramaSymbol (*)(CUIEngine* thisptr, int type, const char* text);
-    using onDeletePanel = void (*)(CUIEngine* thisptr, PanelHandle* panelHandle);
-};
+using CUtlMap = std::map<std::string, int>;
+using CUtlString = std::string;
+using CUtlVector = std::vector<int>;
 
 struct CPanel2D {
-    const void* vmt;
-    CUIPanel* uiPanel;
+public:
+    explicit CPanel2D(const char* id, CUIPanel* parent)
+        : vmt(nullptr), uiPanel(parent) {
+        // Initialize id here
+    }
 
     using Create = CPanel2D*(const char* id, CUIPanel* parent);
 };
 
 struct CLabel : CPanel2D {
+public:
+    explicit CLabel(CPanel2D* parent, const char* id)
+        : CPanel2D(id, parent), text("") {
+        // Initialize other member variables here
+    }
+
     using Constructor = void(CLabel* thisptr, CPanel2D* parent, const char* id);
-    using setTextInternal = void (*)(CLabel* thisptr, const char* value, int textType, bool trustedSource);
+
+    std::string text;
+    using setTextInternal = void (*)(CLabel* thisptr, const std::string& value, int textType, bool trustedSource);
 };
 
-struct LINUX_ARGS([[gnu::packed]]) CStyleSymbol {
-    static constexpr auto kInvalidId{0xFF};
+struct CImagePanel : CPanel2D {
+public:
+    explicit CImagePanel(CPanel2D* parent, const char* id)
+        : CPanel2D(id, parent), m_strSource("") {
+        // Initialize other member variables here
+    }
 
-    [[nodiscard]] bool isValid() const noexcept
-    {
+    using Constructor = void(CImagePanel* thisptr, CPanel2D* parent, const char* id);
+
+    std::string m_strSource;
+    using SetImage = void(CImagePanel* thisptr, const std::string& imageUrl, const std::string& defaultImageUrl, ImageProperties* properties);
+};
+
+struct CStyleSymbol {
+public:
+    static constexpr auto kInvalidId = 0xFF;
+
+    [[nodiscard]] bool isValid() const noexcept {
         return m_Id != kInvalidId;
     }
 
-    std::uint8_t m_Id{kInvalidId};
+    std::uint8_t m_Id = kInvalidId;
 };
 
-struct LINUX_ARGS([[gnu::packed]]) CStyleProperty {
+struct CStyleProperty {
+public:
     const void* vmt;
     CStyleSymbol m_symPropertyName;
     bool m_bDisallowTransition;
 };
 
 struct CUILength {
+public:
     enum EUILengthTypes {
         k_EUILengthUnset,
         k_EUILengthLength,
         k_EUILengthPercent
     };
 
-    [[nodiscard]] static CUILength pixels(float value) noexcept
-    {
+    [[nodiscard]] static CUILength pixels(float value) noexcept {
         return CUILength{value, k_EUILengthLength};
     }
 
-    [[nodiscard]] static CUILength percent(float value) noexcept
-    {
+    [[nodiscard]] static CUILength percent(float value) noexcept {
         return CUILength{value, k_EUILengthPercent};
     }
 
@@ -94,36 +91,41 @@ struct CUILength {
 };
 
 struct CStylePropertyWidth : CStyleProperty {
-    static constexpr auto kName{"width"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyWidth@panorama@@", "N8panorama19CStylePropertyWidthE")};
+public:
+    static constexpr auto kName = "width";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyWidth@panorama@@";
 
     CUILength m_Length;
 };
 
 struct CStylePropertyOpacity : CStyleProperty {
-    static constexpr auto kName{"opacity"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyOpacity@panorama@@", "N8panorama21CStylePropertyOpacityE")};
+public:
+    static constexpr auto kName = "opacity";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyOpacity@panorama@@";
 
     float opacity;
 };
 
 struct CStylePropertyZIndex : CStyleProperty {
-    static constexpr auto kName{"z-index"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyZIndex@panorama@@", "N8panorama20CStylePropertyZIndexE")};
+public:
+    static constexpr auto kName = "z-index";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyZIndex@panorama@@";
 
     float zindex;
 };
 
 struct CStylePropertyHeight : CStyleProperty {
-    static constexpr auto kName{"height"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyHeight@panorama@@", "N8panorama20CStylePropertyHeightE")};
+public:
+    static constexpr auto kName = "height";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyHeight@panorama@@";
 
     CUILength m_Height;
 };
 
 struct CStylePropertyImageShadow : CStyleProperty {
-    static constexpr auto kName{"img-shadow"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyImageShadow@panorama@@", "N8panorama25CStylePropertyImageShadowE")};
+public:
+    static constexpr auto kName = "img-shadow";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyImageShadow@panorama@@";
 
     bool fullySet;
     CUILength horizontalOffset;
@@ -134,8 +136,9 @@ struct CStylePropertyImageShadow : CStyleProperty {
 };
 
 struct CStylePropertyPosition : CStyleProperty {
-    static constexpr auto kName{"position"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyPosition@panorama@@", "N8panorama22CStylePropertyPositionE")};
+public:
+    static constexpr auto kName = "position";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyPosition@panorama@@";
 
     CUILength x;
     CUILength y;
@@ -143,64 +146,68 @@ struct CStylePropertyPosition : CStyleProperty {
 };
 
 struct CStylePropertyTransformOrigin : CStyleProperty {
-    static constexpr auto kName{"transform-origin"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyTransformOrigin@panorama@@", "N8panorama29CStylePropertyTransformOriginE")};
+public:
+    static constexpr auto kName = "transform-origin";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyTransformOrigin@panorama@@";
 
     CUILength x;
     CUILength y;
     bool m_bParentRelative;
 };
 
-enum EHorizontalAlignment : std::uint8_t {
-	k_EHorizontalAlignmentUnset,
-	k_EHorizontalAlignmentLeft,
-	k_EHorizontalAlignmentCenter,
-	k_EHorizontalAlignmentRight
+enum class EHorizontalAlignment : std::uint8_t {
+    k_EHorizontalAlignmentUnset,
+    k_EHorizontalAlignmentLeft,
+    k_EHorizontalAlignmentCenter,
+    k_EHorizontalAlignmentRight
 };
 
-enum EVerticalAlignment : std::uint8_t {
-	k_EVerticalAlignmentUnset,
-	k_EVerticalAlignmentTop,
-	k_EVerticalAlignmentCenter,
-	k_EVerticalAlignmentBottom
+enum class EVerticalAlignment : std::uint8_t {
+    k_EVerticalAlignmentUnset,
+    k_EVerticalAlignmentTop,
+    k_EVerticalAlignmentCenter,
+    k_EVerticalAlignmentBottom
 };
 
 struct CStylePropertyAlign : CStyleProperty {
-    static constexpr auto kName{"align"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyAlign@panorama@@", "N8panorama19CStylePropertyAlignE")};
+public:
+    static constexpr auto kName = "align";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyAlign@panorama@@";
 
     EHorizontalAlignment m_eHorizontalAlignment;
     EVerticalAlignment m_eVerticalAlignment;
 };
 
 struct CStylePropertyWashColor : CStyleProperty {
-    static constexpr auto kName{"wash-color"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyWashColor@panorama@@", "N8panorama23CStylePropertyWashColorE")};
+public:
+    static constexpr auto kName = "wash-color";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyWashColor@panorama@@";
 
     Color color;
     bool fullySet;
 };
 
-enum EFlowDirection : std::uint8_t {
+enum class EFlowDirection : std::uint8_t {
     k_EFlowNone = 1,
     k_EFlowDown,
     k_EFlowRight
 };
 
 struct CStylePropertyFlowChildren : CStyleProperty {
-    static constexpr auto kName{"flow-children"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyFlowChildren@panorama@@", "N8panorama26CStylePropertyFlowChildrenE")};
+public:
+    static constexpr auto kName = "flow-children";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyFlowChildren@panorama@@";
 
     EFlowDirection m_eFlowDirection;
 };
 
-enum EFontStyle : std::int8_t {
+enum class EFontStyle : std::int8_t {
     k_EFontStyleUnset = -1,
     k_EFontStyleNormal = 0,
     k_EFontStyleItalic = 2,
 };
 
-enum EFontWeight : std::int8_t {
+enum class EFontWeight : std::int8_t {
     k_EFontWeightUnset = -1,
     k_EFontWeightNormal = 0,
     k_EFontWeightMedium = 1,
@@ -211,7 +218,7 @@ enum EFontWeight : std::int8_t {
     k_EFontWeightSemiBold = 6,
 };
 
-enum EFontStretch : std::int8_t {
+enum class EFontStretch : std::int8_t {
     k_EFontStretchUnset = -1,
     k_EFontStretchNormal = 0,
     k_EFontStretchCondensed = 1,
@@ -219,8 +226,9 @@ enum EFontStretch : std::int8_t {
 };
 
 struct CStylePropertyFont : CStyleProperty {
-    static constexpr auto kName{"font"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyFont@panorama@@", "N8panorama18CStylePropertyFontE")};
+public:
+    static constexpr auto kName = "font";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyFont@panorama@@";
 
     CUtlString m_strFontFamily;
     float m_flFontSize;
@@ -230,8 +238,9 @@ struct CStylePropertyFont : CStyleProperty {
 };
 
 struct CStylePropertyTextShadow : CStyleProperty {
-    static constexpr auto kName{"text-shadow"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyTextShadow@panorama@@", "N8panorama24CStylePropertyTextShadowE")};
+public:
+    static constexpr auto kName = "text-shadow";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyTextShadow@panorama@@";
 
     bool fullySet;
     CUILength horizontalOffset;
@@ -242,6 +251,7 @@ struct CStylePropertyTextShadow : CStyleProperty {
 };
 
 struct CStylePropertyDimensionsBase : CStyleProperty {
+public:
     CUILength m_left;
     CUILength m_top;
     CUILength m_right;
@@ -249,60 +259,78 @@ struct CStylePropertyDimensionsBase : CStyleProperty {
 };
 
 struct CStylePropertyMargin : CStylePropertyDimensionsBase {
-    static constexpr auto kName{"margin"};
-    static constexpr auto kMangledTypeName{WIN32_LINUX(".?AVCStylePropertyMargin@panorama@@", "N8panorama20CStylePropertyMarginE")};
+public:
+    static constexpr auto kName = "margin";
+    static constexpr auto kMangledTypeName = ".?AVCStylePropertyMargin@panorama@@";
 };
 
 struct CPanelStyle {
+public:
     using SetProperty = void(CPanelStyle* thisptr, CStyleProperty* styleProperty, bool transition);
 
-    using StylePropertySymbols = CUtlMap<CUtlString, CStyleSymbol>;
+    using StylePropertySymbols = CUtlMap<std::string, CStyleSymbol>;
+
+    StylePropertySymbols symbols;
 };
 
 struct CUIPanel {
+public:
     const void* vmt;
     CPanel2D* clientPanel;
 
-    using setParent = void (*)(CUIPanel* thisptr, CUIPanel* parent);
-    using setVisible = void (*)(CUIPanel* thisptr, bool visible);
-    using findChildInLayoutFile = CUIPanel* (*)(CUIPanel* thisptr, const char* childId);
-    using getAttributeString = const char* (*)(CUIPanel* thisptr, CPanoramaSymbol attributeName, const char* defaultValue);
-    using setAttributeString = void (*)(CUIPanel* thisptr, CPanoramaSymbol attributeName, const char* value);
+    using setParent = void(CUIPanel* thisptr, CUIPanel* parent);
+    using setVisible = void(CUIPanel* thisptr, bool visible);
+    using findChildInLayoutFile = CUIPanel*(CUIPanel* thisptr, const std::string& childId);
+    using getAttributeString = const char*(CUIPanel* thisptr, CStyleSymbol attributeName, const std::string& defaultValue);
+    using setAttributeString = void(CUIPanel* thisptr, CStyleSymbol attributeName, const std::string& value);
 
-    using childrenVector = CUtlVector<CUIPanel*>;
-    using classesVector = CUtlVector<CPanoramaSymbol>;
+    using childrenVector = std::vector<CUIPanel*>;
+    using classesVector = std::vector<CStyleSymbol>;
+
+    virtual ~CUIPanel() = default;
+
+    virtual void draw() = 0;
 };
 
 struct ImageProperties {
-    PAD(16);
+public:
     int textureWidth;
     int textureHeight;
     float scale;
 };
 
-struct CImagePanel : CPanel2D {
-    using m_strSource = CUtlString;
-
-    using Constructor = void(CImagePanel* thisptr, CPanel2D* parent, const char* id);
-    using SetImage = void(CImagePanel* thisptr, const char* imageUrl, const char* defaultImageUrl, ImageProperties* properties);
-};
-
 struct CTransform3D {
+public:
     const void* vmt;
+
+    virtual ~CTransform3D() = default;
+
+    virtual void transform() = 0;
 };
 
 struct CTransformTranslate3D : CTransform3D {
+public:
     CUILength m_x;
     CUILength m_y;
     CUILength m_z;
+
+    void transform() override;
 };
 
 struct CTransformScale3D : CTransform3D {
+public:
     Vector m_VecScale;
+
+    void transform() override;
 };
 
 struct CTopLevelWindow {
+public:
     using m_flScaleFactor = float;
+
+    virtual void handleEvent(int eventId) = 0;
 };
 
-}
+} // namespace cs2
+
+#endif // CS2_PANORAMA_H
