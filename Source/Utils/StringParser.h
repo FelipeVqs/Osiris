a24bfda385345b54f9e79677e651298e30708ec3
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:51 +0000

    improve

--- Source/Utils/StringParser.h
+++ Source/Utils/StringParser.h
@@ -1,27 +1,55 @@
 #pragma once
 
+#include <cctype>
+#include <charconv>
 #include <string_view>
+#include <concepts>
 
 class StringParser {
 public:
     explicit StringParser(const char* string) noexcept
-        : string{ string }
+        : string_{ string }
     {
     }
 
+    StringParser(StringParser&& other) noexcept
+        : string_{ other.string_ }
+    {
+        other.string_ = nullptr;
+    }
+
+    StringParser& operator=(StringParser&& other) noexcept
+    {
+        if (this != &other) {
+            string_ = other.string_;
+            other.string_ = nullptr;
+        }
+        return *this;
+    }
+
+    [[nodiscard]] const char* begin() const noexcept { return string_; }
+    [[nodiscard]] const char* end() const noexcept { return string_ + length_; }
+
     [[nodiscard]] std::string_view getLine(char delimiter) noexcept
     {
-        const auto begin = string;
+        const auto begin = string_;
         std::size_t length = 0;
-        while (*string != '\0' && *string++ != delimiter)
+        while (*string_ != '\0' && *string_++ != delimiter)
             ++length;
         return { begin, length };
     }
 
     [[nodiscard]] char getChar() noexcept
     {
-        if (*string != '\0')
-            return *string++;
+        if (*string_ != '\0')
+            return *string_++;
+        return '\0';
+    }
+
+    [[nodiscard]] char peekChar() const noexcept
+    {
+        if (*string_ != '\0')
+            return *string_;
         return '\0';
     }
 
@@ -34,17 +62,17 @@ public:
         bool parseSuccessful = false;
 
         std::size_t currentDigit = 0;
-        while (*string >= '0' && *string <= '9') {
+        while (*string_ >= '0' && *string_ <= '9') {
             if (currentDigit < std::numeric_limits<IntegralType>::digits10) {
                 parsedInteger *= 10;
-                parsedInteger += (*string - '0');
+                parsedInteger += (*string_ - '0');
                 ++currentDigit;
                 parseSuccessful = true;
             } else {
                 parseSuccessful = false;
             }
 
-            ++string;
+            ++string_;
         }
 
         if (parseSuccessful)
@@ -59,27 +87,48 @@ public:
         FloatType parsedFloat{};
         bool parseSuccessful = false;
 
-        if (*string == '-') {
-            // unsupported
-            return false;
+        if (*string_ == '-') {
+            ++string_;
+            if (*string_ < '0' || *string_ > '9')
+                return false;
         }
 
-        while (*string >= '0' && *string <= '9') {
+        while (*string_ >= '0' && *string_ <= '9') {
             parsedFloat *= 10.0f;
-            parsedFloat += static_cast<FloatType>(*string - '0');
+            parsedFloat += static_cast<FloatType>(*string_ - '0');
             parseSuccessful = true;
-            ++string;
+            ++string_;
         }
 
-        if (*string == '.')
-            ++string;
+        if (*string_ == '.') {
+            ++string_;
+            FloatType fraction = 1.0f;
+            while (*string_ >= '0' && *string_ <= '9') {
+                fraction /= 10.0f;
+                parsedFloat += static_cast<FloatType>(*string_ - '0') * fraction;
+                parseSuccessful = true;
+                ++string_;
+            }
+        }
 
-        FloatType fraction = 1.0f;
-        while (*string >= '0' && *string <= '9') {
-            fraction /= 10.0f;
-            parsedFloat += static_cast<FloatType>(*string - '0') * fraction;
-            parseSuccessful = true;
-            ++string;
+        if (*string_ == 'e' || *string_ == 'E') {
+            ++string_;
+            int exponent = 0;
+            bool exponent_is_negative = false;
+            if (*string_ == '-') {
+                exponent_is_negative = true;
+                ++string_;
+            } else if (*string_ == '+') {
+                ++string_;
+            }
+            while (*string_ >= '0' && *string_ <= '9') {
+                exponent = 10 * exponent + (*string_ - '0');
+                ++string_;
+            }
+            if (exponent_is_negative)
+                parsedFloat /= std::pow(10.0f, exponent);
+            else
+                parsedFloat *= std::pow(10.0f, exponent);
         }
 
         if (parseSuccessful)
@@ -89,5 +138,6 @@ public:
     }
 
 private:
-    const char* string;
+    const char* string_;
+    std::size_t length_ = 0;
 };
