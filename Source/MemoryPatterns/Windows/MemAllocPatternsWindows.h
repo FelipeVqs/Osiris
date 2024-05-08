a766075afd23f98c45a2e7db61b57b6e261d83db
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:37 +0000

    improve

--- Source/MemoryPatterns/Windows/MemAllocPatternsWindows.h
+++ Source/MemoryPatterns/Windows/MemAllocPatternsWindows.h
@@ -2,8 +2,26 @@
 
 #include <MemoryPatterns/MemAllocPatterns.h>
 #include <MemorySearch/BytePatternLiteral.h>
+#include <stdexcept>
 
-inline std::int8_t* MemAllocPatterns::allocOffset() const noexcept
+namespace
 {
-    return clientPatternFinder("FF 50 ? 48 89 43 ? 4C 8D"_pat).add(2).as<std::int8_t*>();
+    constexpr BytePatternLiteral clientPattern{"FF 50 ? 48 89 43 ? 4C 8D"_pat};
+}
+
+inline std::int8_t* MemAllocPatterns::allocOffset() const
+{
+    const auto patternResult = clientPattern.find(getClientMemory());
+    if (!patternResult)
+    {
+        throw std::runtime_error("Client pattern not found");
+    }
+
+    const auto offsetResult = patternResult.add(2);
+    if (!offsetResult)
+    {
+        throw std::runtime_error("Failed to calculate offset");
+    }
+
+    return offsetResult.as<std::int8_t*>();
 }
