b2c0580a4ada5f3d065a35d2f542e37607ca3517
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:16 +0000

    improve

--- Source/MemoryPatterns/Linux/SdlPatternsLinux.h
+++ Source/MemoryPatterns/Linux/SdlPatternsLinux.h
@@ -3,7 +3,20 @@
 #include <MemoryPatterns/SdlPatterns.h>
 #include <MemorySearch/BytePatternLiteral.h>
 
-sdl3::SDL_PeepEvents** SdlPatterns::peepEventsPointer(sdl3::SDL_PeepEvents* peepEvents) const noexcept
+/// Returns the address of the \c sdl3::SDL_PeepEvents pointer in memory that matches the given pattern.
+/// \param peepEvents A pointer to the \c sdl3::SDL_PeepEvents function.
+/// \return The address of the \c sdl3::SDL_PeepEvents pointer in memory that matches the given pattern.
+[[nodiscard]] constexpr auto SdlPatterns::peepEventsPointer(sdl3::SDL_PeepEvents* peepEvents) const noexcept
 {
-    return sdlPatternFinder.matchPatternAtAddress((void*)peepEvents, "FF 25 ? ? ? ?"_pat).add(2).abs().as<sdl3::SDL_PeepEvents**>();
+    constexpr auto pattern = BytePatternLiteral::fromString("FF 25 ? ? ? ?"_pat);
+    const auto matchResult = sdlPatternFinder.matchPatternAtAddress((void*)peepEvents, pattern);
+
+    if (!matchResult) {
+        return nullptr;
+    }
+
+    const auto matchAddress = matchResult.getMatchedAddress();
+    const auto offset = matchResult.getPatternOffset();
+
+    return reinterpret_cast<sdl3::SDL_PeepEvents**>(matchAddress + offset + 2);
 }
