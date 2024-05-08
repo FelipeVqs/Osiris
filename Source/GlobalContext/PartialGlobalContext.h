d818d39085f7efdfc7f3598bf5a7f34cc8c74adc
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:53 +0000

    improve

--- Source/GlobalContext/PartialGlobalContext.h
+++ Source/GlobalContext/PartialGlobalContext.h
@@ -7,17 +7,23 @@
 #include <Platform/Macros/PlatformSpecific.h>
 #include <SDL/SdlDll.h>
 
-struct PartialGlobalContext {
-    explicit PartialGlobalContext(SdlDll sdlDll, SdlPatterns sdlPatterns) noexcept
+struct PartialGlobalContext final {
+    // Default constructor
+    PartialGlobalContext() = default;
+
+    // Construct with SdlDll and SdlPatterns
+    constexpr PartialGlobalContext(const SdlDll& sdlDll, const SdlPatterns& sdlPatterns) noexcept
         : peepEventsHook{sdlPatterns.peepEventsPointer(sdlDll.peepEvents())}
     {
     }
 
+    // PeepEventsHook member
     PeepEventsHook peepEventsHook;
 
+    // Enable the hook if it is valid
     void enableIfValid() noexcept
     {
-        if (peepEventsHook.isValid())
+        if (peepEventsHook.isValid() && peepEventsHook.getPeepEvents() != nullptr)
             peepEventsHook.enable();
     }
 };
