a3ce9e6dbb15f62194faedec03a38ba1c2fe3320
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:18 +0000

    improve

--- Source/CS2/Classes/EntitySystem/CEntityIdentity.h
+++ Source/CS2/Classes/EntitySystem/CEntityIdentity.h
@@ -1,7 +1,7 @@
-#pragma once
+#ifndef CENTITYIDENTITY_H
+#define CENTITYIDENTITY_H
 
 #include <Utils/Pad.h>
-
 #include "CEntityHandle.h"
 
 namespace cs2
@@ -10,12 +10,22 @@ namespace cs2
 struct CEntityInstance;
 
 struct CEntityIdentity {
-    CEntityInstance* entity;
+    CEntityInstance* entity{nullptr};
     PAD(8);
     CEntityHandle handle;
     PAD(100);
+
+    CEntityIdentity() : entity(nullptr) {}
+
+    ~CEntityIdentity() {}
+
+    CEntityHandle getHandle() const {
+        return handle;
+    }
 };
 
 static_assert(sizeof(CEntityIdentity) == 120);
 
-}
+} // namespace cs2
+
+#endif // CENTITYIDENTITY_H
