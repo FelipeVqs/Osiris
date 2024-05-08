b30d41735dda4b7a7addab7b7206ed589459aaf5
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:33 +0000

    improve

--- Source/MemoryPatterns/Windows/PanoramaUiEnginePatternsWindows.h
+++ Source/MemoryPatterns/Windows/PanoramaUiEnginePatternsWindows.h
@@ -3,27 +3,55 @@
 #include <MemoryPatterns/PanoramaUiEnginePatterns.h>
 #include <MemorySearch/BytePatternLiteral.h>
 
-inline cs2::CUIEngine::getPanelHandle PanoramaUiEnginePatterns::getPanelHandle() const noexcept
-{
-    return panoramaPatternFinder("8B 81 ? ? ? ? 4C 8B D9 83 F8 FF 74 41"_pat).as<cs2::CUIEngine::getPanelHandle>();
-}
+class PanoramaUiEnginePatterns {
+public:
+    PanoramaUiEnginePatterns() = delete;
+    PanoramaUiEnginePatterns(const PanoramaUiEnginePatterns&) = delete;
+    PanoramaUiEnginePatterns& operator=(const PanoramaUiEnginePatterns&) = delete;
 
-inline cs2::CUIEngine::getPanelPointer PanoramaUiEnginePatterns::getPanelPointer() const noexcept
-{
-    return panoramaPatternFinder("4C 63 02 4D 8B C8"_pat).as<cs2::CUIEngine::getPanelPointer>();
-}
+    PanoramaUiEnginePatterns(PanoramaUiEnginePatterns&&) = delete;
+    PanoramaUiEnginePatterns& operator=(PanoramaUiEnginePatterns&&) = delete;
 
-inline cs2::CUIEngine::runScript PanoramaUiEnginePatterns::runScript() const noexcept
-{
-    return panoramaPatternFinder("48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 54 41 56 41 57 48 81 EC ? ? ? ? 4C 8B"_pat).as<cs2::CUIEngine::runScript>();
-}
+    static cs2::CUIEngine::getPanelHandle getPanelHandle() {
+        return patternFinder("8B 81 ? ? ? ? 4C 8B D9 83 F8 FF 74 41"_pat).as<cs2::CUIEngine::getPanelHandle>();
+    }
 
-inline cs2::CUIEngine::makeSymbol PanoramaUiEnginePatterns::makeSymbol() const noexcept
-{
-    return panoramaPatternFinder("8B CA 49 8B D0"_pat).as<cs2::CUIEngine::makeSymbol>();
-}
+    static cs2::CUIEngine::getPanelPointer getPanelPointer() {
+        return patternFinder("4C 63 02 4D 8B C8"_pat).as<cs2::CUIEngine::getPanelPointer>();
+    }
 
-inline cs2::CUIEngine::onDeletePanel PanoramaUiEnginePatterns::onDeletePanel() const noexcept
-{
-    return panoramaPatternFinder("48 89 5C 24 ? 56 48 83 EC 20 8B 05"_pat).as<cs2::CUIEngine::onDeletePanel>();
+    static cs2::CUIEngine::runScript runScript() {
+        return patternFinder("48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 54 41 56 41 57 48 81 EC ? ? ? ? 4C 8B"_pat).as<cs2::CUIEngine::runScript>();
+    }
+
+    static cs2::CUIEngine::makeSymbol makeSymbol() {
+        return patternFinder("8B CA 49 8B D0"_pat).as<cs2::CUIEngine::makeSymbol>();
+    }
+
+    static cs2::CUIEngine::onDeletePanel onDeletePanel() {
+        return patternFinder("48 89 5C 24 ? 56 48 83 EC 20 8B 05"_pat).as<cs2::CUIEngine::onDeletePanel>();
+    }
+
+private:
+    static BytePatternFinder& patternFinder(const char* pattern) {
+        static std::unordered_map<std::string, BytePatternFinder> patternMap;
+        if (patternMap.find(pattern) == patternMap.end()) {
+            patternMap[pattern] = BytePatternFinder(pattern);
+        }
+        return patternMap[pattern];
+    }
+
+    static BytePatternFinder& panoramaPatternFinder(const char* pattern) {
+        static BytePatternFinder panoramaPattern(pattern);
+        return panoramaPattern;
+    }
+
+    static BytePatternFinder panoramaPattern;
+};
+
+BytePatternFinder PanoramaUiEnginePatterns::panoramaPattern = BytePatternFinder("8B 81 ? ? ? ? 4C 8B D9 83 F8 FF 74 41"_pat);
+
+cs2::CUIEngine::getPanelHandle PanoramaUiEnginePatterns::patternNotFound(const char* pattern) {
+    // Handle the case when the pattern is not found
+    // ...
 }
