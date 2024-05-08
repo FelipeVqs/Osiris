b9ef906de1ab1fde43f4a5cb1fd53195636d5f23
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:29 +0000

    improve

--- Source/FeatureHelpers/LifeState.h
+++ Source/FeatureHelpers/LifeState.h
@@ -3,6 +3,12 @@
 #include <CS2/Classes/Entities/C_BaseEntity.h>
 #include <CS2/Constants/LifeStateConstants.h>
 
-enum class LifeState : cs2::C_BaseEntity::m_lifeState {
+// Use the enum class keyword to make LifeState a strongly-typed enumeration
+// to prevent unintended implicit conversions and improve type safety.
+enum class LifeState : std::underlying_type_t<cs2::C_BaseEntity::m_lifeState> {
     Alive = cs2::LIFE_ALIVE
 };
+
+// Optionally, you can provide a type alias for the underlying type of LifeState
+using LifeStateUnderlyingType = std::underlying_type_t<LifeState>;
+
