c310c26dc6e7eed577128a2330971180f9fce3f4
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:57 +0000

    improve

--- Source/MemoryPatterns/Windows/TopLevelWindowPatternsWindows.h
+++ Source/MemoryPatterns/Windows/TopLevelWindowPatternsWindows.h
@@ -1,11 +1,24 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 
-#include <MemoryPatterns/TopLevelWindowPatterns.h>
-#include <MemorySearch/BytePatternLiteral.h>
+#include "MemoryPatterns/TopLevelWindowPatterns.h"
+#include "MemorySearch/BytePatternLiteral.h"
 
-inline UiScaleFactorOffset TopLevelWindowPatterns::uiScaleFactorOffset() const noexcept
-{
-    return panoramaPatternFinder("F3 0F 11 49 ? 48 8B F9"_pat).add(4).readOffset<UiScaleFactorOffset>();
+namespace {
+constexpr auto PANORAMA_PATTERN = BytePatternLiteral("F3 0F 11 49 ? 48 8B F9");
 }
+
+namespace MemoryPatterns {
+
+inline UiScaleFactorOffset TopLevelWindowPatterns::uiScaleFactorOffset() const noexcept {
+    auto pattern_finder = std::make_unique<PatternFinder>(PANORAMA_PATTERN);
+    const auto pattern_address = pattern_finder->find();
+    if (!pattern_address) {
+        return 0;
+    }
+    return pattern_address.add(4).readOffset<UiScaleFactorOffset>();
+}
+
+}  // namespace MemoryPatterns
