a103496a9872fa009008ce216e919113185e0aa0
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:25:01 +0000

    improve

--- Source/Platform/Windows/RTTI/RttiCompleteObjectLocator.h
+++ Source/Platform/Windows/RTTI/RttiCompleteObjectLocator.h
@@ -1,6 +1,10 @@
 #pragma once
 
+// Adding a comment to describe the purpose of the struct
 struct RttiCompleteObjectLocator {
-    static constexpr auto kOffsetOfOffsetInCompleteClass{4};
-    static constexpr auto kOffsetOfTypeDescriptorRva{12};
+    // Using an enum class to improve readability and avoid naming collisions
+    enum class Offset : std::uint32_t {
+        kOffsetOfOffsetInCompleteClass = 4,
+        kOffsetOfTypeDescriptorRva = 12
+    };
 };
