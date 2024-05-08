098e3bdf27375a9a2e0e6c2e96760018c379223c
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:43 +0000

    improve

--- Source/MemoryPatterns/Windows/EntitySystemPatternsWindows.h
+++ Source/MemoryPatterns/Windows/EntitySystemPatternsWindows.h
@@ -2,18 +2,48 @@
 
 #include <MemoryPatterns/EntitySystemPatterns.h>
 #include <MemorySearch/BytePatternLiteral.h>
+#include <optional>
+#include <system_error>
 
-inline cs2::CGameEntitySystem** EntitySystemPatterns::entitySystem() const noexcept
+namespace
 {
-    return clientPatternFinder("48 8B 0D ? ? ? ? 48 8D 94 24 ? ? ? ? 33 DB"_pat).add(3).abs().as<cs2::CGameEntitySystem**>();
+    constexpr auto& entitySystemPattern = "48 8B 0D ? ? ? ? 48 8D 94 24 ? ? ? ? 33 DB"_pat;
+    constexpr auto& highestEntityIndexOffsetPattern = "3B 8F ? ? ? ? 7E 06"_pat;
+    constexpr auto& entityListOffsetPattern = "48 8D 4B ? E8 ? ? ? ? 8D 85"_pat;
 }
 
-inline HighestEntityIndexOffset EntitySystemPatterns::highestEntityIndexOffset() const noexcept
+inline std::optional<cs2::CGameEntitySystem**> EntitySystemPatterns_entitySystem(EntitySystemPatterns& self) noexcept
 {
-    return clientPatternFinder("3B 8F ? ? ? ? 7E 06"_pat).add(2).readOffset<HighestEntityIndexOffset>();
+    try
+    {
+        return self.clientPatternFinder(entitySystemPattern).add(3).abs().as<cs2::CGameEntitySystem**>();
+    }
+    catch (const std::exception&)
+    {
+        return std::nullopt;
+    }
 }
 
-inline EntityListOffset EntitySystemPatterns::entityListOffset() const noexcept
+inline std::optional<cs2::HighestEntityIndexOffset> EntitySystemPatterns_highestEntityIndexOffset(EntitySystemPatterns& self) noexcept
 {
-    return clientPatternFinder("48 8D 4B ? E8 ? ? ? ? 8D 85"_pat).add(3).readOffset<EntityListOffset>();
+    try
+    {
+        return self.clientPatternFinder(highestEntityIndexOffsetPattern).add(2).readOffset<cs2::HighestEntityIndexOffset>();
+    }
+    catch (const std::exception&)
+    {
+        return std::nullopt;
+    }
+}
+
+inline std::optional<cs2::EntityListOffset> EntitySystemPatterns_entityListOffset(EntitySystemPatterns& self) noexcept
+{
+    try
+    {
+        return self.clientPatternFinder(entityListOffsetPattern).add(3).readOffset<cs2::EntityListOffset>();
+    }
+    catch (const std::exception&)
+    {
+        return std::nullopt;
+    }
 }
