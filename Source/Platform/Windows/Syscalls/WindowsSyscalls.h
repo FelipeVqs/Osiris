4aa0a713f68fedb9ccf5d3df7be7d6c649f8b0af
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:07 +0000

    improve

--- Source/Platform/Windows/Syscalls/WindowsSyscalls.h
+++ Source/Platform/Windows/Syscalls/WindowsSyscalls.h
@@ -6,37 +6,37 @@
 #include "SyscallParams.h"
 #include "WindowsSyscallIndex.h"
 
-extern "C" std::size_t systemCall(const SyscallParams* params, ...);
+extern "C" std::size_t systemCall(const SyscallParams& params, ...);
 
 struct IO_APC_ROUTINE;
 
-struct WindowsSyscalls {
-    static NTSTATUS NtCreateFile(
-        HANDLE* fileHandle,
-        ACCESS_MASK desiredAccess,
-        OBJECT_ATTRIBUTES* objectAttributes,
-        IO_STATUS_BLOCK* ioStatusBlock,
-        LARGE_INTEGER* allocationSize,
-        ULONG fileAttributes,
-        ULONG shareAccess,
-        ULONG createDisposition,
-        ULONG createOptions,
-        void* eaBuffer,
-        ULONG eaLength) noexcept
-    {
-        const SyscallParams params{WindowsSyscallIndex::NtCreateFile, reinterpret_cast<std::uintptr_t>(fileHandle)};
-        return static_cast<NTSTATUS>(systemCall(&params, desiredAccess, objectAttributes, ioStatusBlock, allocationSize, fileAttributes, shareAccess, createDisposition, createOptions, eaBuffer, eaLength));
+struct WindowsSyscalls final {
+    template <typename... Args>
+    static NTSTATUS NtCreateFile(Args... args) noexcept {
+        const SyscallParams params{WindowsSyscallIndex::NtCreateFile, 0};
+        return static_cast<NTSTATUS>(systemCall(params, args...));
     }
 
-    static NTSTATUS NtReadFile(HANDLE fileHandle, HANDLE event, IO_APC_ROUTINE* apcRoutine, void* apcContext, IO_STATUS_BLOCK* ioStatusBlock, void* buffer, ULONG length, LARGE_INTEGER* byteOffset, ULONG* key) noexcept
-    {
-        const SyscallParams params{WindowsSyscallIndex::NtReadFile, reinterpret_cast<std::uintptr_t>(fileHandle)};
-        return static_cast<NTSTATUS>(systemCall(&params, event, apcRoutine, apcContext, ioStatusBlock, buffer, length, byteOffset, key));
+    template <typename... Args>
+    static NTSTATUS NtReadFile(Args... args) noexcept {
+        const SyscallParams params{WindowsSyscallIndex::NtReadFile, 0};
+        return static_cast<NTSTATUS>(systemCall(params, args...));
     }
 
-    static NTSTATUS NtClose(HANDLE handle) noexcept
-    {
+    static NTSTATUS NtClose(HANDLE handle) noexcept {
         const SyscallParams params{WindowsSyscallIndex::NtClose, reinterpret_cast<std::uintptr_t>(handle)};
-        return static_cast<NTSTATUS>(systemCall(&params));
+        return static_cast<NTSTATUS>(systemCall(params));
+    }
+
+private:
+    template <typename... Args>
+    static std::enable_if_t<sizeof...(Args) == 10, NTSTATUS> systemCallWrapper(const SyscallParams& params, Args... args) noexcept {
+        return static_cast<NTSTATUS>(systemCall(params, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args>
+    static std::enable_if_t<sizeof...(Args) != 10, NTSTATUS> systemCallWrapper(const SyscallParams&) noexcept {
+        static_assert(sizeof...(Args) == 1, "Invalid number of arguments");
+        return 0;
     }
 };
