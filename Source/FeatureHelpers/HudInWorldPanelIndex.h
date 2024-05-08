84c108efd57c70d3604d99540b18a0d8430093c6
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:20 +0000

    improve

--- Source/FeatureHelpers/HudInWorldPanelIndex.h
+++ Source/FeatureHelpers/HudInWorldPanelIndex.h
@@ -1,5 +1,8 @@
+// Header file for in-world HUD element indices
+
 #pragma once
 
 #include <cstdint>
 
-using HudInWorldPanelIndex = std::uint16_t;
+/// Represents an index for a HUD element that is rendered within the game world.
+using WorldHudElementIndex = std::uint16_t;
