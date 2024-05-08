8683202bd99fbbed8e3da99436cbf1e719b3e310
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:34 +0000

    improve

--- Source/Platform/Windows/DLLs/Shell32Dll.h
+++ Source/Platform/Windows/DLLs/Shell32Dll.h
@@ -1,16 +1,32 @@
 #pragma once
 
 #include <ShlObj_core.h>
-
+#include <optional>
 #include <Platform/Windows/WindowsDynamicLibrary.h>
 
 struct Shell32Dll : WindowsDynamicLibrary {
+public:
     Shell32Dll() : WindowsDynamicLibrary{ "shell32.dll" }
     {
+        if (!isLoaded()) {
+            throw std::runtime_error("Failed to load shell32.dll");
+        }
     }
 
-    [[nodiscard]] auto SHGetKnownFolderPath() const noexcept
-    {
-        return getFunctionAddress("SHGetKnownFolderPath").as<decltype(&::SHGetKnownFolderPath)>();
+    explicit Shell32Dll(bool load) : Shell32Dll() {
+        if (load) {
+            load();
+        }
+    }
+
+    [[nodiscard]] std::optional<decltype(&::SHGetKnownFolderPath)> SHGetKnownFolderPath() const noexcept {
+        if (auto addr = getFunctionAddress("SHGetKnownFolderPath")) {
+            return addr.as<decltype(&::SHGetKnownFolderPath)>();
+        }
+        return std::nullopt;
+    }
+
+    void load() const {
+        loadLibrary();
     }
 };
