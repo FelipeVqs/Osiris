6d00635b0ed7020f04fdd5b834add0d1fc0dbda9
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:37 +0000

    improve

--- Source/HookDependencies/HookDependenciesBuilder.h
+++ Source/HookDependencies/HookDependenciesBuilder.h
@@ -3,13 +3,11 @@
 #include <FeatureHelpers/ConVarFinder.h>
 #include <FeatureHelpers/FeatureHelpers.h>
 #include <GameClasses/Implementation/GameClassImplementations.h>
-
 #include "HookDependenciesMask.h"
 
 struct HookDependenciesBuilder {
-    [[nodiscard]] HookDependenciesMask getConVarAccessor() const noexcept
-    {
-        if (requiredDependencies.has<ConVarAccessor>()) {
+    [[nodiscard]] HookDependenciesMask getConVarAccessor() const {
+        if (requiredDependencies.has<ConVarAccessor>) {
             if (featureHelpers.conVars.has_value())
                 return HookDependenciesMask{}.set<ConVarAccessor>();
 
@@ -23,36 +21,32 @@ struct HookDependenciesBuilder {
         return {};
     }
 
-    [[nodiscard]] HookDependenciesMask getFileSystem(cs2::CBaseFileSystem** fileSystem) const noexcept
-    {
-        if (requiredDependencies.has<FileSystem>() && featureHelpers.fileSystem) {
+    [[nodiscard]] virtual HookDependenciesMask getFileSystem(cs2::CBaseFileSystem** fileSystem) const {
+        if (requiredDependencies.has<FileSystem> && featureHelpers.fileSystem) {
             if ((*fileSystem = *featureHelpers.fileSystem) != nullptr)
                 return HookDependenciesMask{}.set<FileSystem>();
         }
         return {};
     }
 
-    [[nodiscard]] HookDependenciesMask getSoundChannels(cs2::SoundChannels** soundChannels) const noexcept
-    {
-        if (requiredDependencies.has<SoundChannels>() && featureHelpers.soundChannels) {
+    [[nodiscard]] virtual HookDependenciesMask getSoundChannels(cs2::SoundChannels** soundChannels) const {
+        if (requiredDependencies.has<SoundChannels> && featureHelpers.soundChannels) {
             if ((*soundChannels = *featureHelpers.soundChannels) != nullptr)
                 return HookDependenciesMask{}.set<SoundChannels>();
         }
         return {};
     }
 
-    [[nodiscard]] HookDependenciesMask getPlantedC4(cs2::CPlantedC4** plantedC4) const noexcept
-    {
-        if (requiredDependencies.has<PlantedC4>() && featureHelpers.plantedC4s && featureHelpers.plantedC4s->size > 0) {
+    [[nodiscard]] virtual HookDependenciesMask getPlantedC4(cs2::CPlantedC4** plantedC4) const {
+        if (requiredDependencies.has<PlantedC4> && featureHelpers.plantedC4s && featureHelpers.plantedC4s->size > 0) {
             if ((*plantedC4 = featureHelpers.plantedC4s->memory[0]) != nullptr)
                 return HookDependenciesMask{}.set<PlantedC4>();
         }
         return {};
     }
 
-    [[nodiscard]] HookDependenciesMask getCurTime(float* curTime) const noexcept
-    {
-        if (requiredDependencies.has<CurTime>() && featureHelpers.globalVars) {
+    [[nodiscard]] virtual HookDependenciesMask getCurTime(float* curTime) const {
+        if (requiredDependencies.has<CurTime> && featureHelpers.globalVars) {
             if (const auto globalVars = *featureHelpers.globalVars) {
                 *curTime = globalVars->curtime;
                 return HookDependenciesMask{}.set<CurTime>();
@@ -61,18 +55,16 @@ struct HookDependenciesBuilder {
         return {};
     }
 
-    [[nodiscard]] HookDependenciesMask getFovScale(float* fovScale) const noexcept
-    {
-        if (requiredDependencies.has<FovScale>()) {
+    [[nodiscard]] virtual HookDependenciesMask getFovScale(float* fovScale) const {
+        if (requiredDependencies.has<FovScale>) {
             *fovScale = featureHelpers.viewToProjectionMatrix.getFovScale();
             return HookDependenciesMask{}.set<FovScale>();
         }
         return {};
     }
 
-    [[nodiscard]] HookDependenciesMask getLocalPlayerController(cs2::CCSPlayerController** localPlayerController) const noexcept
-    {
-        if (requiredDependencies.has<LocalPlayerController>() && featureHelpers.localPlayerController) {
+    [[nodiscard]] virtual HookDependenciesMask getLocalPlayerController(cs2::CCSPlayerController** localPlayerController) const {
+        if (requiredDependencies.has<LocalPlayerController> && featureHelpers.localPlayerController) {
             *localPlayerController = *featureHelpers.localPlayerController;
             if (*localPlayerController)
                 return HookDependenciesMask{}.set<LocalPlayerController>();
@@ -80,8 +72,7 @@ struct HookDependenciesBuilder {
         return {};
     }
 
-    [[nodiscard]] HookDependenciesMask getEntityList(const cs2::CConcreteEntityList** entityList, cs2::CEntityIndex* highestEntityIndex) const noexcept
-    {
+    [[nodiscard]] HookDependenciesMask getEntityList(const cs2::CConcreteEntityList** entityList, cs2::CEntityIndex* highestEntityIndex) const {
         if (!requiredDependencies.has<EntityListWalker>() && !requiredDependencies.has<EntityFromHandleFinder>())
             return {};
 
@@ -92,7 +83,7 @@ struct HookDependenciesBuilder {
         if (!entitySystem)
             return {};
 
-        if (requiredDependencies.has<EntityListWalker>() && gameClassImplementations.entitySystem.highestEntityIndexOffset) {
+        if (requiredDependencies.has<EntityListWalker> && gameClassImplementations.entitySystem.highestEntityIndexOffset) {
             *highestEntityIndex = *gameClassImplementations.entitySystem.highestEntityIndexOffset.of(entitySystem).get();
             if (!highestEntityIndex->isValid())
                 *highestEntityIndex = cs2::kMaxValidEntityIndex;
