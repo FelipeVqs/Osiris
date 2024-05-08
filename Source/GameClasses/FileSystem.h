e519cdcc4f76e94df59d7222bafd899432951412
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:56 +0000

    improve

--- Source/GameClasses/FileSystem.h
+++ Source/GameClasses/FileSystem.h
@@ -4,19 +4,26 @@
 #include "FileNameSymbolTable.h"
 #include "Implementation/FileSystemImpl.h"
 
+// Use this alias to avoid having to write cs2::CBaseFileSystem repeatedly.
+using CS2FileSystem = cs2::CBaseFileSystem;
+
 struct FileSystem {
-    explicit FileSystem(cs2::CBaseFileSystem& thisptr, const FileSystemImpl& impl) noexcept
+    // Initialize member variables in the constructor initializer list.
+    explicit FileSystem(CS2FileSystem& thisptr, const FileSystemImpl& impl) noexcept
         : thisptr{thisptr}
         , impl{impl}
-    {
-    }
+    {}
 
-    [[nodiscard]] FileNameSymbolTable fileNames() const noexcept
-    {
+    // Add a const qualifier to indicate that this method does not modify the object.
+    [[nodiscard]] FileNameSymbolTable fileNames() const noexcept {
+        // Explain the purpose of this method.
+        /**
+         * @return A FileNameSymbolTable object that maps file names to file offsets.
+         */
         return FileNameSymbolTable{impl.fileNamesOffset.of(&thisptr).get()};
     }
 
 private:
-    cs2::CBaseFileSystem& thisptr;
+    CS2FileSystem& thisptr;
     const FileSystemImpl& impl;
 };
