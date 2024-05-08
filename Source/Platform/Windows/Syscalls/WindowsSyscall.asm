41a395a6bdfec46408e748b3413164b1e09e93e4
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:33 +0000

    improve

--- Source/Platform/Windows/Syscalls/WindowsSyscall.asm
+++ Source/Platform/Windows/Syscalls/WindowsSyscall.asm
@@ -1,10 +1,38 @@
 .CODE
 
-systemCall PROC
-    mov rax, [rcx] ; load syscall index into rax
-    mov r10, [rcx+8] ; load first parameter into r10
+syscallWrapper PROC
+    ; Save non-volatile registers that we are going to modify
+    push r11
+    push rdi
+
+    ; Load syscall index into rax
+    mov eax, dword ptr [rcx]
+
+    ; Check if the syscall index is valid
+    cmp eax, MAX_SYSCALL_INDEX
+    jae syscallWrapperEnd ; If it's not, jump to the end of the procedure
+
+    ; Load first parameter into rdi
+    mov rdi, rcx
+    add rdi, 8
+
+    ; Check if the first parameter is NULL
+    cmp rdi, 0
+    je syscallWrapperNullParam ; If it is, jump to the error handling code
+
     syscall
+
+syscallWrapperEnd:
+    ; Restore non-volatile registers
+    pop rdi
+    pop r11
     ret
-systemCall ENDP
+
+syscallWrapperNullParam:
+    ; Set error code and return
+    mov eax, E_NULL_PARAM
+    jmp syscallWrapperEnd
+
+syscallWrapper ENDP
 
 END
