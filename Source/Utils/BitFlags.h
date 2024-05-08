0e24ec485ae44a9616f9d83ec0102a91d54bed07
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:24 +0000

    improve

--- Source/Utils/BitFlags.h
+++ Source/Utils/BitFlags.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include <concepts>
+#include <iostream>
 #include <limits>
 #include <type_traits>
 
-template <typename Flag, std::integral StorageType>
-    requires std::is_enum_v<Flag>
+template <typename Flag, typename StorageType = std::underlying_type_t<Flag>,
+          typename = std::enable_if_t<std::is_enum_v<Flag>>>
 class BitFlags {
 public:
+    constexpr BitFlags() noexcept = default;
+
     template <Flag flag>
     constexpr void set() noexcept
     {
@@ -31,13 +34,22 @@ public:
         return flags != 0;
     }
 
-private:
-    template <Flag flag>
-    static constexpr auto toMask() noexcept
+    friend std::ostream& operator<<(std::ostream& os, const BitFlags& flags)
     {
-        static_assert(static_cast<std::underlying_type_t<Flag>>(flag) >= 0);
-        static_assert(static_cast<std::underlying_type_t<Flag>>(flag) < std::numeric_limits<StorageType>::digits);
-        return 1 << static_cast<std::underlying_type_t<Flag>>(flag);
+        os << flags.flags;
+        return os;
+    }
+
+private:
+    static_assert(std::numeric_limits<StorageType>::digits >= std::numeric_limits<Flag>::max(),
+                  "StorageType must have enough bits to represent all Flag values.");
+
+    template <Flag flag>
+    static constexpr StorageType toMask() noexcept
+    {
+        static_assert(static_cast<StorageType>(flag) >= 0);
+        static_assert(static_cast<StorageType>(flag) < std::numeric_limits<StorageType>::digits);
+        return 1 << static_cast<StorageType>(flag);
     }
 
     StorageType flags{0};
