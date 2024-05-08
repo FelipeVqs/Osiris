220331ee3f373a49d9edac7c127d54b72fd26a98
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:17 +0000

    improve

--- Source/CS2/Classes/CEntitySubclassVDataBase.h
+++ Source/CS2/Classes/CEntitySubclassVDataBase.h
@@ -1,8 +1,20 @@
 #pragma once
 
+// Include any necessary headers here.
+
 namespace cs2
 {
 
-struct CEntitySubclassVDataBase {};
+// Provide a more complete definition for the struct.
+struct CEntitySubclassVDataBase
+{
+    // Include any necessary fields or methods here.
 
-}
+    // For example:
+    int m\_field1;
+    float m\_field2;
+
+    void SomeMethod();
+};
+
+} // namespace cs2
