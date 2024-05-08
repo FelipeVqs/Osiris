65e285801aec2a38e5bc0b73d87c32d795591d3f
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:11 +0000

    improve

--- Source/Utils/FlagsBuilder.h
+++ Source/Utils/FlagsBuilder.h
@@ -7,29 +7,43 @@
 template <std::integral T, typename Flag>
 class FlagsBuilder {
 public:
+    constexpr FlagsBuilder(T flags = T{}) noexcept : flags(flags) {}
+
     template <Flag flag>
-    constexpr FlagsBuilder& set(bool b) noexcept
-    {
+    constexpr FlagsBuilder& set(bool b) noexcept {
         static_assert(std::in_range<T>(rawType<flag>()));
         if (b)
             flags |= static_cast<T>(flag);
         return *this;
     }
 
-    [[nodiscard]] T get() noexcept
-    {
+    [[nodiscard]] T get() const noexcept {
         return flags;
+    }
+
+    constexpr FlagsBuilder& reset() noexcept {
+        flags = T{};
+        return *this;
+    }
+
+    template <Flag flag>
+    constexpr bool isSet() const noexcept {
+        return (flags & rawType<flag>()) != T{};
     }
 
 private:
     template <Flag flag>
-    static constexpr auto rawType() noexcept
-    {
-        if constexpr (std::is_enum_v<Flag>)
+    static constexpr auto rawType() noexcept {
+        if constexpr (std::is_enum_v<Flag>) {
             return static_cast<std::underlying_type_t<Flag>>(flag);
-        else
-            return flag;
+        } else {
+            if constexpr (std::is_integral_v<Flag>) {
+                return flag;
+            } else {
+                static_assert(!std::is_same_v<Flag, Flag>, "Unsupported Flag type");
+            }
+        }
     }
 
-    T flags{};
+    T flags;
 };
