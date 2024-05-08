8d0d09d19a53634be9c316c72064d02bb7b48da3
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:37 +0000

    improve

--- Source/Platform/Macros/IsCompiler.h
+++ Source/Platform/Macros/IsCompiler.h
@@ -1,9 +1,22 @@
 #pragma once
 
+// Define a macro for each supported compiler
 #if defined(__clang__)
-    #define CLANG
+    #define HAS_CLANG 1
 #elif defined(__GNUC__)
-    #define GCC
+    #define HAS_GCC 1
 #elif defined(_MSC_VER)
-    #define MSVC
+    #define HAS_MSVC 1
+#else
+    #error "Unsupported compiler"
 #endif
+
+// Use the macros in the code
+#if HAS_CLANG
+    // Code specific to Clang
+#elif HAS_GCC
+    // Code specific to GCC
+#elif HAS_MSVC
+    // Code specific to MSVC
+#endif
+
