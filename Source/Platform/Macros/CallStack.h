52d15ad6c7ae8c87f81eeabf8fea3c746ac475a7
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:35 +0000

    improve

--- Source/Platform/Macros/CallStack.h
+++ Source/Platform/Macros/CallStack.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <type_traits>
 
 #include "IsPlatform.h"
 #include <Utils/ReturnAddress.h>
@@ -9,17 +10,55 @@
 
 #include <intrin.h>
 
-#define RETURN_ADDRESS() ReturnAddress{_ReturnAddress()}
-#define FRAME_ADDRESS() (std::uintptr_t(_AddressOfReturnAddress()) - sizeof(std::uintptr_t))
+#define HAS_RETURN_ADDRESS _has_return_address
+#define HAS_FRAME_ADDRESS __has_builtin(__builtin_frame_address)
 
 #elif IS_LINUX()
 
-#define RETURN_ADDRESS() ReturnAddress{__builtin_return_address(0)}
-#define FRAME_ADDRESS() std::uintptr_t(__builtin_frame_address(0))
+#define HAS_RETURN_ADDRESS __has_builtin(__builtin_return_address)
+#define HAS_FRAME_ADDRESS 1
+
+#else
+
+#define HAS_RETURN_ADDRESS 0
+#define HAS_FRAME_ADDRESS 0
+
+#endif
+
+#if HAS_RETURN_ADDRESS
+
+#define RETURN_ADDRESS() ReturnAddress<std::integral_constant<bool, HAS_RETURN_ADDRESS>>{_ReturnAddress()}
 
 #else
 
 #define RETURN_ADDRESS()
-#define FRAME_ADDRESS()
 
 #endif
+
+#if HAS_FRAME_ADDRESS
+
+#define FRAME_ADDRESS() std::uintptr_t(const_cast<std::uintptr_t*>(__builtin_frame_address(0)))
+
+#define FRAME_ADDRESS_CONSTEXPR() std::uintptr_t(__builtin_frame_address(0))
+
+#else
+
+#define FRAME_ADDRESS()
+#define FRAME_ADDRESS_CONSTEXPR()
+
+#endif
+
+namespace Utils {
+
+template <typename T = std::true_type>
+class ReturnAddress {
+public:
+    constexpr ReturnAddress(void* address) noexcept : address_(address) {}
+
+    void* get() const noexcept { return address_; }
+
+private:
+    void* address_;
+};
+
+}  // namespace Utils
