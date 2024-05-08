56a423afdfafa5e883907f94b57f6bb975dad017
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:22 +0000

    improve

--- Source/GameClasses/OffsetTypes/GameRulesOffset.h
+++ Source/GameClasses/OffsetTypes/GameRulesOffset.h
@@ -3,7 +3,16 @@
 #include <CS2/Classes/C_CSGameRules.h>
 #include <Utils/FieldOffset.h>
 
-template <typename FieldType, typename OffsetType>
+/**
+ * A type alias for a field offset in the CSGameRules class.
+ * This allows for easier access to the field's value using the offset.
+ */
+template <typename FieldType, typename OffsetType = std::int32_t>
 using GameRulesOffset = FieldOffset<cs2::C_CSGameRules, FieldType, OffsetType>;
 
-using RoundStartTimeOffset = GameRulesOffset<cs2::C_CSGameRules::m_fRoundStartTime, std::int8_t>;
+/**
+ * A type alias for the offset of the m_fRoundStartTime field in the CSGameRules class.
+ */
+constexpr GameRulesOffset<float, std::int32_t> RoundStartTimeOffset{
+    offsetof(cs2::C_CSGameRules, m_fRoundStartTime)
+};
