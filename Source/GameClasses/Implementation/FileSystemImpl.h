25adc86d9a667fc4db20745fd22c9a9209422838
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:58 +0000

    improve

--- Source/GameClasses/Implementation/FileSystemImpl.h
+++ Source/GameClasses/Implementation/FileSystemImpl.h
@@ -1,12 +1,1 @@
-#pragma once
-
-#include <MemoryPatterns/FileSystemPatterns.h>
-
-struct FileSystemImpl {
-    explicit FileSystemImpl(const FileSystemPatterns& fileSystemPatterns) noexcept
-        : fileNamesOffset{fileSystemPatterns.fileNamesOffset()}
-    {
-    }
-
-    FileNamesOffset fileNamesOffset;
-};
+>
