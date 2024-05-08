3e44c8223cebeb71f0b531602e1cc916bae6a1fb
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:12 +0000

    improve

--- Source/SDL/SdlConstants.h
+++ Source/SDL/SdlConstants.h
@@ -1,9 +1,19 @@
 #pragma once
 
-namespace sdl3
-{
+namespace sdl_message_box_type {
 
-constexpr auto SDL_MESSAGEBOX_ERROR{0x10};
-constexpr auto SDL_MESSAGEBOX_WARNING{0x20};
+constexpr int INFORMATION = 0x0;
+constexpr int ERROR = 0x10;
+constexpr int WARNING = 0x20;
+constexpr int QUESTION = 0x30;
 
-}
+} // namespace sdl_message_box_type
+
+namespace sdl3 {
+
+using message_box_type = int;
+
+inline const message_box_type ERROR = sdl_message_box_type::ERROR;
+inline const message_box_type WARNING = sdl_message_box_type::WARNING;
+
+} // namespace sdl3
