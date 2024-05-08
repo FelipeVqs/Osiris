1e65ce12446be6b50fcb2a80d6ff771556db7911
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:40 +0000

    improve

--- Source/MemoryPatterns/Linux/WeaponVDataPatternsLinux.h
+++ Source/MemoryPatterns/Linux/WeaponVDataPatternsLinux.h
@@ -3,7 +3,21 @@
 #include <MemoryPatterns/WeaponVDataPatterns.h>
 #include <MemorySearch/BytePatternLiteral.h>
 
-inline OffsetToWeaponName WeaponVDataPatterns::offsetToWeaponName() const noexcept
+inline OffsetToWeaponName WeaponVDataPatterns::getWeaponNameOffset() const noexcept
 {
-    return clientPatternFinder("74 ? 48 8B ? ? ? ? ? 48 8D ? ? ? ? ? BE ? ? ? ? 8B 3D ? ? ? ? 48 8D"_pat).add(5).readOffset<OffsetToWeaponName>();
+    // Search for the pattern with a maximum match distance of 16 bytes
+    constexpr int maxMatchDistance = 16;
+    auto patternResult = clientPatternFinder("74 ? 48 8B ? ? ? ? ? 48 8D ? ? ? ? ? BE ? ? ? ? 8B 3D ? ? ? ? 48 8D"_pat, maxMatchDistance);
+
+    // If the pattern was not found, return a default offset value of 0
+    if (!patternResult) {
+        return OffsetToWeaponName(0);
+    }
+
+    // Move the read position to the next byte after the pattern match
+    patternResult->add(5);
+
+    // Read the offset value from memory
+    return patternResult->readOffset<OffsetToWeaponName>();
 }
+
