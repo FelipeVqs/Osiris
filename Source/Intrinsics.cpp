9494a95c3da84b52024a2448ef226a5c48288208
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:02 +0000

    improve

--- Source/Intrinsics.cpp
+++ Source/Intrinsics.cpp
@@ -6,7 +6,7 @@ extern "C" int _fltused{ 0x9875 };
 namespace intrinsics
 {
 
-[[nodiscard]] inline int memcmp(const unsigned char* lhs, const unsigned char* rhs, std::size_t count) noexcept
+[[nodiscard]] constexpr inline int memcmp(const std::byte* lhs, const std::byte* rhs, std::size_t count) noexcept
 {
     for (std::size_t i = 0; i < count; ++i) {
         if (lhs[i] != rhs[i])
@@ -15,21 +15,21 @@ namespace intrinsics
     return 0;
 }
 
-inline std::byte* memcpy(std::byte* dest, const std::byte* src, std::size_t count) noexcept
+constexpr inline std::byte* memcpy(std::byte* dest, const std::byte* src, std::size_t count) noexcept
 {
     for (std::size_t i = 0; i < count; ++i)
         dest[i] = src[i];
     return dest;
 }
 
-inline unsigned char* memset(unsigned char* const dest, unsigned char ch, std::size_t count) noexcept
+constexpr inline std::byte* memset(std::byte* dest, std::byte ch, std::size_t count) noexcept
 {
     for (std::size_t i = 0; i < count; ++i)
         dest[i] = ch;
     return dest;
 }
 
-[[nodiscard]] inline int strcmp(const unsigned char* s1, const unsigned char* s2) noexcept
+[[nodiscard]] constexpr inline int strcmp(const char* s1, const char* s2) noexcept
 {
     while (*s1 == *s2) {
         if (*s1 == '\0')
@@ -41,7 +41,7 @@ inline unsigned char* memset(unsigned char* const dest, unsigned char ch, std::s
     return *s1 - *s2;
 }
 
-inline unsigned char* memmove(unsigned char* dest, const unsigned char* src, std::size_t count) noexcept
+constexpr inline std::byte* memmove(std::byte* dest, const std::byte* src, std::size_t count) noexcept
 {
     if (dest < src) {
         for (std::size_t i = 0; i < count; ++i)
@@ -53,7 +53,7 @@ inline unsigned char* memmove(unsigned char* dest, const unsigned char* src, std
     return dest;
 }
 
-inline std::size_t strlen(const char* str) noexcept
+constexpr inline std::size_t strlen(const char* str) noexcept
 {
     std::size_t length = 0;
     while (*str) {
@@ -63,7 +63,7 @@ inline std::size_t strlen(const char* str) noexcept
     return length;
 }
 
-inline const unsigned char* memchr(const unsigned char* ptr, unsigned char ch, std::size_t count) noexcept
+constexpr inline const std::byte* memchr(const std::byte* ptr, std::byte ch, std::size_t count) noexcept
 {
     for (std::size_t i = 0; i < count; ++i) {
         if (ptr[i] == ch)
@@ -72,46 +72,39 @@ inline const unsigned char* memchr(const unsigned char* ptr, unsigned char ch, s
     return nullptr;
 }
 
-}
+} // namespace intrinsics
 
-#pragma function(memcpy)
-void* memcpy(void* dest, const void* src, size_t count)
+constexpr void* memcpy(void* dest, const void* src, std::size_t count)
 {
     return intrinsics::memcpy(static_cast<std::byte*>(dest), static_cast<const std::byte*>(src), count);
 }
 
-#pragma function(memmove)
-void* memmove(void* dest, const void* src, size_t count)
+constexpr void* memmove(void* dest, const void* src, std::size_t count)
 {
-    return intrinsics::memmove(static_cast<unsigned char*>(dest), static_cast<const unsigned char*>(src), count);
+    return intrinsics::memmove(static_cast<std::byte*>(dest), static_cast<const std::byte*>(src), count);
 }
 
-#pragma function(memcmp)
-int memcmp(const void* s1, const void* s2, std::size_t n)
+constexpr int memcmp(const void* s1, const void* s2, std::size_t n)
 {
-    return intrinsics::memcmp(static_cast<const unsigned char*>(s1), static_cast<const unsigned char*>(s2), n);
+    return intrinsics::memcmp(static_cast<const std::byte*>(s1), static_cast<const std::byte*>(s2), n);
 }
 
-#pragma function(memset)
-void* memset(void* dest, int ch, size_t count)
+constexpr void* memset(void* dest, int ch, std::size_t count)
 {
-    return intrinsics::memset(static_cast<unsigned char*>(dest), static_cast<unsigned char>(ch), count);
+    return intrinsics::memset(static_cast<std::byte*>(dest), static_cast<std::byte>(ch), count);
 }
 
-#pragma function(strcmp)
-int strcmp(const char* s1, const char* s2)
+constexpr int strcmp(const char* s1, const char* s2)
 {
-    return intrinsics::strcmp(reinterpret_cast<const unsigned char*>(s1), reinterpret_cast<const unsigned char*>(s2));
+    return intrinsics::strcmp(s1, s2);
 }
 
-#pragma function(strlen)
-std::size_t strlen(const char* str)
+constexpr std::size_t strlen(const char* str)
 {
     return intrinsics::strlen(str);
 }
 
-#pragma function(memchr)
-const void* memchr(const void* ptr, int ch, std::size_t count)
+constexpr const void* memchr(const void* ptr, int ch, std::size_t count)
 {
-    return intrinsics::memchr(reinterpret_cast<const unsigned char*>(ptr), static_cast<unsigned char>(ch), count);
+    return intrinsics::memchr(static_cast<const std::byte*>(ptr), static_cast<std::byte>(ch), count);
 }
