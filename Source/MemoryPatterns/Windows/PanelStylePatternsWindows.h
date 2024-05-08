3996a13a767e2aa3e20fabd0f95cf37a66b2e0a0
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:48 +0000

    improve

--- Source/MemoryPatterns/Windows/PanelStylePatternsWindows.h
+++ Source/MemoryPatterns/Windows/PanelStylePatternsWindows.h
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <cstddef>
+#include <stdexcept>
 
 #include <MemoryPatterns/PanelStylePatterns.h>
 #include <MemorySearch/BytePatternLiteral.h>
 
-inline cs2::CPanelStyle::SetProperty* PanelStylePatterns::setProperty() const noexcept
-{
-    return panoramaPatternFinder("E8 ? ? ? ? 8B 45 B8"_pat).add(1).abs().as<cs2::CPanelStyle::SetProperty*>();
+inline cs2::CPanelStyle::SetProperty* PanelStylePatterns::getProperty() const {
+    if (const auto patternResult = panoramaPatternFinder("E8 ? ? ? ? 8B 45 B8"_pat)) {
+        const auto offset = patternResult.add(1).abs();
+        return offset.as<cs2::CPanelStyle::SetProperty*>();
+    }
+    throw std::runtime_error("Property pattern not found");
 }
 
-inline cs2::CPanelStyle::StylePropertySymbols* PanelStylePatterns::stylePropertiesSymbols() const noexcept
-{
-    if (const auto pointerToStylePropertySymbolsMemory{panoramaPatternFinder("48 8B 05 ? ? ? ? 48 63 FB"_pat).add(3).abs().as<std::byte*>()})
-        return reinterpret_cast<cs2::CPanelStyle::StylePropertySymbols*>(pointerToStylePropertySymbolsMemory - offsetof(cs2::CPanelStyle::StylePropertySymbols, memory));
+inline const cs2::CPanelStyle::StylePropertySymbols* PanelStylePatterns::stylePropertiesSymbols() const {
+    if (const auto pointerToStylePropertySymbolsMemory = panoramaPatternFinder("48 8B 05 ? ? ? ? 48 63 FB"_pat)) {
+        const auto offset = patternResult.add(3).abs();
+        const auto memoryAddress = offset.as<std::byte*>();
+        const auto stylePropertySymbolsAddress = reinterpret_cast<cs2::CPanelStyle::StylePropertySymbols*>(memoryAddress - offsetof(cs2::CPanelStyle::StylePropertySymbols, memory));
+        return stylePropertySymbolsAddress;
+    }
     return nullptr;
 }
