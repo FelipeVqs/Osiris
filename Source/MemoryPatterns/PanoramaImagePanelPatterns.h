44da1aee76d3b74a348d233638647b6a771dcc60
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:48 +0000

    improve

--- Source/MemoryPatterns/PanoramaImagePanelPatterns.h
+++ Source/MemoryPatterns/PanoramaImagePanelPatterns.h
@@ -1,18 +1,34 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 
 #include <CS2/Classes/Panorama.h>
 #include <GameClasses/OffsetTypes/PanoramaImagePanelOffset.h>
 #include <Helpers/PatternNotFoundLogger.h>
 #include <MemorySearch/PatternFinder.h>
 
-struct PanoramaImagePanelPatterns {
-    [[nodiscard]] cs2::CImagePanel::SetImage* setImage() const noexcept;
-    [[nodiscard]] cs2::CImagePanel::Constructor* constructor() const noexcept;
+class PanoramaImagePanelPatterns {
+public:
+    PanoramaImagePanelPatterns(const PatternFinder<PatternNotFoundLogger>& clientPatternFinder) noexcept;
+
+    [[nodiscard]] std::unique_ptr<cs2::CImagePanel::SetImage> setImage() const;
+    [[nodiscard]] std::unique_ptr<cs2::CImagePanel::Constructor> constructor() const;
     [[nodiscard]] std::uint32_t* size() const noexcept;
     [[nodiscard]] ImagePropertiesOffset imagePropertiesOffset() const noexcept;
     [[nodiscard]] OffsetToImagePath offsetToImagePath() const noexcept;
 
+private:
     const PatternFinder<PatternNotFoundLogger>& clientPatternFinder;
 };
+
+PanoramaImagePanelPatterns::PanoramaImagePanelPatterns(const PatternFinder<PatternNotFoundLogger>& clientPatternFinder) noexcept
+    : clientPatternFinder(clientPatternFinder) {}
+
+std::unique_ptr<cs2::CImagePanel::SetImage> PanoramaImagePanelPatterns::setImage() const {
+    // Implementation goes here
+}
+
+std::unique_ptr<cs2::CImagePanel::Constructor> PanoramaImagePanelPatterns::constructor() const {
+    // Implementation goes here
+}
