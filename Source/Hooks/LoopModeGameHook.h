a32c14fa5b18ea33bed5814a7482f29e76f38f48
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:41 +0000

    improve

--- Source/Hooks/LoopModeGameHook.h
+++ Source/Hooks/LoopModeGameHook.h
@@ -4,39 +4,49 @@
 #include <MemoryPatterns/ClientPatterns.h>
 #include <Utils/RefCountedHook.h>
 #include <Vmt/VmtSwapper.h>
+#include <memory>
 
 extern "C" void* LoopModeGameHook_getWorldSession_asm(cs2::CLoopModeGame* thisptr) noexcept;
 
 class LoopModeGameHook : public RefCountedHook<LoopModeGameHook> {
 public:
     LoopModeGameHook(const ClientPatterns& clientPatterns, const VmtLengthCalculator& vmtLengthCalculator) noexcept
-        : loopModeGame{clientPatterns.loopModeGame()}
-        , vmtLengthCalculator{vmtLengthCalculator}
+        : vmtLengthCalculator{vmtLengthCalculator}
+        , loopModeGame{std::make_unique<cs2::CLoopModeGame*>(clientPatterns.loopModeGame())}
     {
     }
+
+    ~LoopModeGameHook() override {
+        uninstall();
+    }
+
+    LoopModeGameHook(const LoopModeGameHook&) = delete;
+    LoopModeGameHook& operator=(const LoopModeGameHook&) = delete;
 
 private:
-    void uninstall() const noexcept
-    {
-        if (loopModeGame && *loopModeGame)
+    void uninstall() const noexcept {
+        if (loopModeGame && *loopModeGame) {
             hook.uninstall(*reinterpret_cast<std::uintptr_t**>(*loopModeGame));
+        }
     }
 
-    [[nodiscard]] bool isInstalled() const noexcept
-    {
-        return hook.wasEverInstalled() && loopModeGame && *loopModeGame && hook.isInstalled(*reinterpret_cast<std::uintptr_t**>(*loopModeGame));
+    [[nodiscard]] bool isInstalled() const noexcept {
+        if (loopModeGame && *loopModeGame && hook.wasEverInstalled()) {
+            return hook.isInstalled(*reinterpret_cast<std::uintptr_t**>(*loopModeGame));
+        }
+        return false;
     }
 
-    void install() noexcept
-    {
-        if (loopModeGame && *loopModeGame && hook.install(vmtLengthCalculator, *reinterpret_cast<std::uintptr_t**>(*loopModeGame))) {
+    void install() noexcept {
+        if (loopModeGame && *loopModeGame) {
+            hook.install(vmtLengthCalculator, *reinterpret_cast<std::uintptr_t**>(*loopModeGame));
             originalGetWorldSession = hook.hook(0, &LoopModeGameHook_getWorldSession_asm);
         }
     }
 
     friend class RefCountedHook;
 
-    cs2::CLoopModeGame** loopModeGame;
+    std::unique_ptr<cs2::CLoopModeGame> loopModeGame;
     VmtLengthCalculator vmtLengthCalculator;
     VmtSwapper hook;
 public:
