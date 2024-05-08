2a46f3e4ec7d6559a72986bb8d805332235b1fb9
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:05 +0000

    improve

--- Source/GameClasses/OffsetTypes/PanoramaUiPanelOffset.h
+++ Source/GameClasses/OffsetTypes/PanoramaUiPanelOffset.h
@@ -1,14 +1,42 @@
 #pragma once
 
 #include <cstdint>
-
-#include <CS2/Classes/Panorama.h>
+#include <cs2/Classes/Panorama.h>
 #include <Utils/FieldOffset.h>
 
+// Template for getting the offset of a field in a class
 template <typename FieldType, typename OffsetType>
 using PanoramaUiPanelOffset = FieldOffset<cs2::CUIPanel, FieldType, OffsetType>;
 
-using ChildPanelsVectorOffset = PanoramaUiPanelOffset<cs2::CUIPanel::childrenVector, std::int8_t>;
-using PanelClassesVectorOffset = PanoramaUiPanelOffset<cs2::CUIPanel::classesVector, std::int32_t>;
-using PanelStyleOffset = PanoramaUiPanelOffset<cs2::CPanelStyle, std::int8_t>;
-using ParentWindowOffset = PanoramaUiPanelOffset<cs2::CTopLevelWindow*, std::int8_t>;
+// Offset of the `childrenVector` field in `cs2::CUIPanel`
+using ChildPanelsVectorOffset = PanoramaUiPanelOffset<cs2::CUIPanel::childrenVector, std::uintptr_t>;
+
+// Offset of the `classesVector` field in `cs2::CUIPanel`
+using PanelClassesVectorOffset = PanoramaUiPanelOffset<cs2::CUIPanel::classesVector, std::uintptr_t>;
+
+// Offset of the `panelStyle` field in `cs2::CUIPanel`
+using PanelStyleOffset = PanoramaUiPanelOffset<cs2::CPanelStyle*, std::uintptr_t>;
+
+// Offset of the `parentWindow` field in `cs2::CTopLevelWindow`
+using ParentWindowOffset = PanoramaUiPanelOffset<cs2::CTopLevelWindow*, std::uintptr_t>;
+
+// Check that `FieldType` is a pointer type
+template <typename T>
+constexpr bool is_pointer() {
+    return std::is_pointer<T>::value;
+}
+
+template <typename FieldType, typename OffsetType>
+using ImprovedPanoramaUiPanelOffset = PanoramaUiPanelOffset<FieldType, OffsetType>;
+
+template <typename FieldType>
+using ImprovedChildPanelsVectorOffset = std::enable_if_t<is_pointer<FieldType>::value, PanoramaUiPanelOffset<cs2::CUIPanel::childrenVector, std::uintptr_t>>;
+
+template <typename FieldType>
+using ImprovedPanelClassesVectorOffset = std::enable_if_t<is_pointer<FieldType>::value, PanoramaUiPanelOffset<cs2::CUIPanel::classesVector, std::uintptr_t>>;
+
+template <typename FieldType>
+using ImprovedPanelStyleOffset = std::enable_if_t<is_pointer<FieldType>::value, PanoramaUiPanelOffset<cs2::CPanelStyle, std::uintptr_t>>;
+
+template <typename FieldType>
+using ImprovedParentWindowOffset = std::enable_if_t<is_pointer<FieldType>::value, PanoramaUiPanelOffset<cs2::CTopLevelWindow, std::uintptr_t>>;
