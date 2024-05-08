df1658c741966a3460817b014922788f7337e219
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:04 +0000

    improve

--- Source/Utils/InRange.h
+++ Source/Utils/InRange.h
@@ -2,54 +2,23 @@
 
 #include <array>
 #include <bit>
-#include <cassert>
 #include <cstddef>
+#include <type_traits>
 
-// workaround for clang < 18 not supporting float non-type template parameters
-template <typename T>
-struct BinaryRepresentationTemplateParameter {
-    std::array<std::byte, sizeof(T)> binaryRepresentation;
+template <typename T, T Min, T Max>
+struct InRange {
+    static_assert(Min <= Max, "Min must be less than or equal to Max");
 
-    explicit(false) constexpr BinaryRepresentationTemplateParameter(T value) noexcept
-        : binaryRepresentation{std::bit_cast<decltype(binaryRepresentation)>(value)}
+    explicit(false) constexpr InRange(T Value) noexcept
+        : value{Value}
     {
+        static_assert(Value >= Min && Value <= Max, "Value must be in the range [Min, Max]");
     }
-
-    template <typename U>
-    BinaryRepresentationTemplateParameter(U) = delete;
 
     constexpr operator T() const noexcept
     {
-        return std::bit_cast<T>(binaryRepresentation);
-    }
-};
-
-template <typename T, BinaryRepresentationTemplateParameter<T> min, BinaryRepresentationTemplateParameter<T> max>
-    requires (min <= max)
-struct InRange {
-    explicit(false) constexpr InRange(T value) noexcept
-        : value{value}
-    {
-        assert(value >= min);
-        assert(value <= max);
-        if (std::is_constant_evaluated()) {
-            if (value < min || value > max)
-                fail();
-        }
-    }
-
-    constexpr operator T&() noexcept
-    {
         return value;
     }
-
-    constexpr operator const T&() const noexcept
-    {
-        return value;
-    }
-
-private:
-    void fail() {}
 
     T value;
 };
