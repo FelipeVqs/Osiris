41945efbcfd43f7d5e16c76af5006af800086a7f
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:38 +0000

    improve

--- Source/Utils/GenericPointer.h
+++ Source/Utils/GenericPointer.h
@@ -1,13 +1,20 @@
 #pragma once
 
+template <typename T>
+concept Pointer = std::is_pointer_v<T>;
+
 struct GenericPointer {
-    template <typename T>
-    explicit(false) GenericPointer(T* pointer) noexcept
-        : value{(void*)pointer}
+    template <Pointer Ptr>
+    explicit(false) GenericPointer(Ptr pointer) noexcept
+        : value(pointer)
     {
     }
 
     GenericPointer() = default;
+    GenericPointer(const GenericPointer&) = delete;
+    GenericPointer& operator=(const GenericPointer&) = delete;
+
+    ~GenericPointer() = default;
 
     [[nodiscard]] explicit operator bool() const noexcept
     {
@@ -17,7 +24,25 @@ struct GenericPointer {
     template <typename T>
     [[nodiscard]] T as() const noexcept
     {
-        return T(value);
+        static_assert<Pointer<T*>, "T must be a pointer type");
+        return *static_cast<T*>(value);
+    }
+
+    T* get() const noexcept
+    {
+        return static_cast<T*>(value);
+    }
+
+    void reset() noexcept
+    {
+        value = nullptr;
+    }
+
+    T* release() noexcept
+    {
+        T* temp = get();
+        value = nullptr;
+        return temp;
     }
 
 private:
