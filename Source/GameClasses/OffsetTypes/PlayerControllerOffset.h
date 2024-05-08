c162235deeeea49ac3a3187bea3e24f42847d1d3
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:08 +0000

    improve

--- Source/GameClasses/OffsetTypes/PlayerControllerOffset.h
+++ Source/GameClasses/OffsetTypes/PlayerControllerOffset.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <type_traits>
 
 #include <CS2/Classes/Entities/CCSPlayerController.h>
 #include <Utils/FieldOffset.h>
@@ -8,5 +9,12 @@
 template <typename FieldType, typename OffsetType>
 using PlayerControllerOffset = FieldOffset<cs2::CCSPlayerController, FieldType, OffsetType>;
 
-using OffsetToPlayerPawnHandle = PlayerControllerOffset<cs2::CCSPlayerController::m_hPawn, std::int32_t>;
-using OffsetToPlayerColor = PlayerControllerOffset<cs2::CCSPlayerController::m_iCompTeammateColor, std::int32_t>;
+using PlayerController = cs2::CCSPlayerController;
+
+using OffsetToPlayerPawnHandle = PlayerControllerOffset<PlayerController::m_hPawn, std::uint32_t>;
+static_assert(OffsetToPlayerPawnHandle::offset >= 0, "Offset must be non-negative");
+
+using OffsetToPlayerColor = PlayerControllerOffset<PlayerController::m_iCompTeammateColor, std::uint32_t>;
+static_assert(OffsetToPlayerColor::offset >= 0, "Offset must be non-negative");
+
+using PlayerColor = std::underlying_type_t<PlayerController::TeammateColor>;
