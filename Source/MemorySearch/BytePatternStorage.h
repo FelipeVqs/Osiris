c2051d1edcc2d66cc025f8c0ea65a8b72fd595e0
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:55 +0000

    improve

--- Source/MemorySearch/BytePatternStorage.h
+++ Source/MemorySearch/BytePatternStorage.h
@@ -10,8 +10,10 @@
 #include "PatternStringWildcard.h"
 
 template <std::size_t Capacity>
-struct BytePatternStorage {
-    explicit(false) consteval BytePatternStorage(const char (&patternString)[Capacity])
+struct BytePatternStorage final {
+    static_assert(Capacity > 0, "Capacity must be greater than zero");
+
+    BytePatternStorage(const char (&patternString)[Capacity]) noexcept
     {
         BytePatternConverter converter{patternString};
         const auto [convertedPattern, error] = converter();
@@ -27,5 +29,6 @@ struct BytePatternStorage {
     std::size_t size{0};
 
 private:
-    void errorOccured();
+    void errorOccured() const noexcept;
 };
+
