2457850ef770a4dce2ed36fdc12a329e8a8e616c
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:30 +0000

    improve

--- Source/FeatureHelpers/StylePropertySymbolMap.h
+++ Source/FeatureHelpers/StylePropertySymbolMap.h
@@ -4,18 +4,18 @@
 #include <CS2/Classes/Panorama.h>
 #include <Platform/Macros/FunctionAttributes.h>
 
-struct StylePropertySymbolMap {
-    [[nodiscard]] [[NOINLINE]] cs2::CStyleSymbol findSymbol(const char* stylePropertyName) const noexcept
+struct StylePropertySymbolMap final {
+    [[nodiscard]] [[NOINLINE]] constexpr cs2::CStyleSymbol findSymbol(const char* stylePropertyName) const noexcept final
     {
         if (!symbols)
             return {};
 
-        for (int i = 0; i < symbols->numElements; ++i) {
+        for (int i = 0; i < symbols->numElements - 1; ++i) {
             if (std::strcmp(symbols->memory[i].key.m_pString, stylePropertyName) == 0)
                 return symbols->memory[i].value;
         }
         return {};
     }
 
-    const cs2::CPanelStyle::StylePropertySymbols* symbols;
+    const cs2::CPanelStyle::StylePropertySymbols* restrict symbols;
 };
