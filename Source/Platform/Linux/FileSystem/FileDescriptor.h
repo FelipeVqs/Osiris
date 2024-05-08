6439b0a64f26e93eb20243fb4dd959fc8a6c7949
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:35 +0000

    improve

--- Source/Platform/Linux/FileSystem/FileDescriptor.h
+++ Source/Platform/Linux/FileSystem/FileDescriptor.h
@@ -1,34 +1,66 @@
-#pragma once
+#ifndef FILE_DESCRIPTOR_H
+#define FILE_DESCRIPTOR_H
 
+#include <cassert>
 #include <Platform/Linux/LinuxPlatformApi.h>
 
-struct FileDescriptor {
+class noncopyable {
+protected:
+    constexpr noncopyable() noexcept = default;
+    ~noncopyable() noexcept = default;
+
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator=(const noncopyable&) = delete;
+};
+
+struct FileDescriptor : private noncopyable {
+public:
     explicit FileDescriptor(int fileDescriptor) noexcept
-        : fileDescriptor{fileDescriptor}
+        : fileDescriptor_{fileDescriptor}
     {
     }
 
-    FileDescriptor(const FileDescriptor&) = delete;
-    FileDescriptor(FileDescriptor&&) = delete;
-    FileDescriptor& operator=(const FileDescriptor&) = delete;
-    FileDescriptor& operator=(FileDescriptor&&) = delete;
+    FileDescriptor(FileDescriptor&& other) noexcept
+        : fileDescriptor_{other.fileDescriptor_}
+    {
+        other.fileDescriptor_ = -1;
+    }
+
+    FileDescriptor& operator=(FileDescriptor&& other) noexcept
+    {
+        if (this != &other) {
+            close();
+            fileDescriptor_ = other.fileDescriptor_;
+            other.fileDescriptor_ = -1;
+        }
+        return *this;
+    }
 
     [[nodiscard]] int get() const noexcept
     {
-        return fileDescriptor;
+        return fileDescriptor_;
     }
 
     [[nodiscard]] bool isValid() const noexcept
     {
-        return fileDescriptor >= 0;
+        return fileDescriptor_ >= 0;
+    }
+
+    void close() noexcept
+    {
+        if (isValid()) {
+            LinuxPlatformApi::close(fileDescriptor_);
+            fileDescriptor_ = -1;
+        }
     }
 
     ~FileDescriptor() noexcept
     {
-        if (isValid())
-            LinuxPlatformApi::close(fileDescriptor);
+        close();
     }
 
 private:
-    int fileDescriptor;
+    int fileDescriptor_ = -1;
 };
+
+#endif // FILE_DESCRIPTOR_H
