039d4380b36058ef5a1a098a15a9daf96df59138
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:15 +0000

    improve

--- Source/GameClasses/Implementation/GameRulesImpl.h
+++ Source/GameClasses/Implementation/GameRulesImpl.h
@@ -3,12 +3,36 @@
 #include <MemoryPatterns/GameRulesPatterns.h>
 
 struct GameRulesImpl {
+private:
     explicit GameRulesImpl(const GameRulesPatterns& gameRulesPatterns) noexcept
-        : roundStartTimeOffset{gameRulesPatterns.roundStartTimeOffset()}
-    {
-    }
-
-    [[nodiscard]] static const GameRulesImpl& instance() noexcept;
+        : roundStartTimeOffset{gameRulesPatterns.roundStartTimeOffset()} {}
 
     RoundStartTimeOffset roundStartTimeOffset;
+
+public:
+    static const GameRulesImpl& instance() noexcept {
+        static GameRulesImpl instance{GameRulesPatterns::getDefault()};
+        return instance;
+    }
+
+    [[nodiscard]] RoundStartTimeOffset getRoundStartTimeOffset() const noexcept {
+        return roundStartTimeOffset;
+    }
+
+    static const GameRulesPatterns& getDefaultGameRulesPatterns() noexcept {
+        return GameRulesPatterns::getDefault();
+    }
+
+    friend class GameRulesFactory;
+};
+
+class GameRulesFactory {
+public:
+    static const GameRulesImpl& getInstance() noexcept {
+        return GameRulesImpl::instance();
+    }
+
+    static const GameRulesPatterns& getDefaultGameRulesPatterns() noexcept {
+        return GameRulesImpl::getDefaultGameRulesPatterns();
+    }
 };
