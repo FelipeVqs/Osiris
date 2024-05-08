b43411d0b40be1ed4246cc9b676930ded0ec5f80
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:39 +0000

    improve

--- Source/Platform/Macros/IsPlatform.h
+++ Source/Platform/Macros/IsPlatform.h
@@ -1,16 +1,24 @@
 #pragma once
 
-#if defined(_WIN64) && defined(__linux__)
-    #error "Conflicting platform macros defined"
-#endif
-
 #if defined(_WIN64)
     #define IS_WIN64() true
     #define IS_LINUX() false
-#elif defined(__linux__)
+#elif defined(__linux__) && defined(__x86_64__)
     #define IS_WIN64() false
     #define IS_LINUX() true
 #else
     #define IS_WIN64() false
     #define IS_LINUX() false
+#endif
+
+#if IS_WIN64()
+    #define HAVE_WIN64 1
+#elif IS_LINUX()
+    #define HAVE_LINUX 1
+#endif
+
+#if HAVE_WIN64
+    // Windows-specific code here
+#elif HAVE_LINUX
+    // Linux-specific code here
 #endif
