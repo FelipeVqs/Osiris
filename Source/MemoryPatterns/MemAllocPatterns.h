0cb76914a3589aa328e80e1246cb90bc703615b9
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:04 +0000

    improve

--- Source/MemoryPatterns/MemAllocPatterns.h
+++ Source/MemoryPatterns/MemAllocPatterns.h
@@ -1,13 +1,25 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 
 #include <CS2/Classes/IMemAlloc.h>
 #include <Helpers/PatternNotFoundLogger.h>
 #include <MemorySearch/PatternFinder.h>
 
-struct MemAllocPatterns {
+class MemAllocPatterns {
+public:
+    MemAllocPatterns(const std::shared_ptr<IMemAlloc>& memAlloc,
+                     const PatternFinder<PatternNotFoundLogger>& clientPatternFinder)
+        : memAlloc_(memAlloc), clientPatternFinder_(clientPatternFinder) {}
+
     [[nodiscard]] std::int8_t* allocOffset() const noexcept;
 
-    const PatternFinder<PatternNotFoundLogger>& clientPatternFinder;
+private:
+    std::shared_ptr<IMemAlloc> memAlloc_;
+    const PatternFinder<PatternNotFoundLogger>& clientPatternFinder_;
 };
+
+std::int8_t* MemAllocPatterns::allocOffset() const noexcept {
+    // Implement the functionality here
+}
