77c08d80785194a80ddde26b788d813bb76ac7b6
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:29 +0000

    improve

--- Source/LinuxEndpoints.s
+++ Source/LinuxEndpoints.s
@@ -3,25 +3,35 @@
 .globl ViewRenderHook_onRenderStart_asm
 
 SDLHook_PeepEvents_asm:
-    push %rdi # backup volatile registers used in the original function
+    push %rbp    ; backup non-volatile rbp used in the original function
+    mov %rsp, %rbp ; set up our own stack frame
+    push %rdi    ; backup volatile registers used in the original function
     push %rsi
     push %rdx
     push %rcx
     push %r8
+    sub $8, %rsp  ; allocate 8 bytes on the stack for the return address
+    mov %rax, (%rsp) ; save the original return address
     call SDLHook_PeepEvents_cpp
+    mov (%rsp), %rax ; restore the original return address
+    add $8, %rsp  ; deallocate the 8 bytes on the stack
     pop %r8
     pop %rcx
     pop %rdx
     pop %rsi
     pop %rdi
-    jmp *%rax # jump to the original function
+    pop %rbp     ; restore the original rbp
+    jmp *%rax    ; jump to the original function
 
 LoopModeGameHook_getWorldSession_asm:
-    push %rdi # backup volatile rdi used in the original function
-    mov 8(%rsp), %rdi # load return address into rdi
+    push %rbp    ; backup non-volatile rbp used in the original function
+    mov %rsp, %rbp ; set up our own stack frame
+    push %rdi    ; backup volatile rdi used in the original function
+    mov 8(%rsp), %rdi ; load return address into rdi
     call LoopModeGameHook_getWorldSession_cpp
     pop %rdi
-    jmp *%rax # jump to the original function
+    pop %rbp     ; restore the original rbp
+    jmp *%rax    ; jump to the original function
 
 ViewRenderHook_onRenderStart_asm:
     jmp ViewRenderHook_onRenderStart_cpp
