baa16438a142771fd5608522fc246e4bc98a52ce
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:52 +0000

    improve

--- Source/MemoryPatterns/WeaponServicesPatterns.h
+++ Source/MemoryPatterns/WeaponServicesPatterns.h
@@ -5,7 +5,16 @@
 #include <MemorySearch/PatternFinder.h>
 
 struct WeaponServicesPatterns {
+    WeaponServicesPatterns(const PatternFinder<PatternNotFoundLogger>& clientPatternFinder) noexcept
+        : clientPatternFinder(clientPatternFinder) {}
+
     [[nodiscard]] OffsetToActiveWeapon offsetToActiveWeapon() const noexcept;
 
     const PatternFinder<PatternNotFoundLogger>& clientPatternFinder;
 };
+
+// Implementation of the member function
+OffsetToActiveWeapon WeaponServicesPatterns::offsetToActiveWeapon() const noexcept {
+    // Implementation using clientPatternFinder to find the offset
+    // ...
+}
