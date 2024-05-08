f4ec8f0f88fa36f93e8cd02fd74c968c9892e6a2
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:14 +0000

    improve

--- Source/MemoryPatterns/Linux/PanelPatternsLinux.h
+++ Source/MemoryPatterns/Linux/PanelPatternsLinux.h
@@ -2,8 +2,13 @@
 
 #include <MemoryPatterns/PanelPatterns.h>
 #include <MemorySearch/BytePatternLiteral.h>
+#include <type_traits>
 
-inline cs2::CPanel2D::Create* PanelPatterns::create() const noexcept
+template <typename T>
+using EnableIfNonConst = std::enable_if_t<!std::is_const_v<T>, T>;
+
+inline EnableIfNonConst<cs2::CPanel2D::Create*> PanelPatterns::create() noexcept
 {
-    return clientPatternFinder("55 48 89 E5 41 56 49 89 FE 41 55 41 54 49 89 F4 48 83 EC 08 48 85 F6 74 0C 48 8B 06 48 89 F7 FF 50 50 49 89 C4 BF ? ? ? ? E8 ? ? ? ? 4C 89 F1"_pat).as<cs2::CPanel2D::Create*>();
+    constexpr auto pattern = BytePatternLiteral("55 48 89 E5 41 56 49 89 FE 41 55 41 54 49 89 F4 48 83 EC 08 48 85 F6 74 0C 48 8B 06 48 89 F7 FF 50 50 49 89 C4 BF ? ? ? ? E8 ? ? ? ? 4C 89 F1"_pat);
+    return static_cast<EnableIfNonConst<cs2::CPanel2D::Create*>>(clientPatternFinder(pattern).as<cs2::CPanel2D::Create*>());
 }
