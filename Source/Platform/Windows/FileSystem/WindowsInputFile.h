6c82873c3a3596bbb606e97226ac7e9e005d2401
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:46 +0000

    improve

--- Source/Platform/Windows/FileSystem/WindowsInputFile.h
+++ Source/Platform/Windows/FileSystem/WindowsInputFile.h
@@ -8,14 +8,24 @@
 #include <Platform/Windows/NtHandle.h>
 #include "WindowsFileSystem.h"
 
+using Byte = std::byte;
+
 struct WindowsInputFile {
     explicit WindowsInputFile(UNICODE_STRING fileName) noexcept
         : handle{WindowsFileSystem::openFileForReading(fileName)}
     {
+        if (!handle) {
+            // Handle error
+        }
     }
 
-    [[nodiscard]] std::size_t read(std::size_t fileOffset, std::span<std::byte> buffer) const noexcept
+    [[nodiscard]] std::size_t read(std::size_t fileOffset, std::span<Byte> buffer) const noexcept
     {
+        if (!handle) {
+            // Handle error
+            return 0;
+        }
+
         return WindowsFileSystem::readFile(handle.get(), fileOffset, buffer);
     }
 
