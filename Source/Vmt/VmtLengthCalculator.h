2c8b0ce25a43227926eb69e41dabba67b34b81a2
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:47 +0000

    improve

--- Source/Vmt/VmtLengthCalculator.h
+++ Source/Vmt/VmtLengthCalculator.h
@@ -6,26 +6,25 @@
 #include "VmtLength.h"
 #include <Utils/MemorySection.h>
 
-struct VmtLengthCalculator {
-    explicit VmtLengthCalculator(MemorySection codeSection, MemorySection vmtSection)
+struct VmtLengthCalculator final {
+    explicit VmtLengthCalculator(const MemorySection& codeSection, const MemorySection& vmtSection)
         : codeSection{ codeSection }, vmtSection{ vmtSection }
-    {
-    }
+    {}
 
-    [[nodiscard]] VmtLength operator()(const std::uintptr_t* vmt) const noexcept
-    {
+    [[nodiscard]] VmtLength operator()(const std::uintptr_t* vmt) const {
         std::size_t length = 0;
-        while (isVmtEntry(vmt + length))
+        while (isVmtEntry(vmt + length)) {
             ++length;
+        }
         return VmtLength{ length };
     }
 
 private:
-    [[nodiscard]] bool isVmtEntry(const std::uintptr_t* pointer) const noexcept
-    {
-        return vmtSection.contains(std::uintptr_t(pointer), sizeof(std::uintptr_t)) && codeSection.contains(*pointer);
+    [[nodiscard]] constexpr static bool isVmtEntry(const std::uintptr_t* pointer) const {
+        return vmtSection.contains(std::uintptr_t(pointer), sizeof(std::uintptr_t)) &&
+               codeSection.contains(*pointer);
     }
 
-    MemorySection codeSection;
-    MemorySection vmtSection;
+    const MemorySection& codeSection;
+    const MemorySection& vmtSection;
 };
