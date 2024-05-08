37468f18a0d06b108abdc9ea7a65e9be145a3d5f
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:23 +0000

    improve

--- Source/Features/Sound/Details/BombPlantSound.h
+++ Source/Features/Sound/Details/BombPlantSound.h
@@ -7,12 +7,11 @@
 #include <CS2/Constants/SoundNames.h>
 
 struct BombPlantSound {
-    static constexpr auto kFadeAwayStart = 2.1f;
-    static constexpr auto kFadeAwayDuration = 0.4f;
-    static constexpr auto kMinScale = 0.4f;
+    static constexpr float kFadeAwayStart = 2.1f;
+    static constexpr float kFadeAwayDuration = 0.4f;
+    static constexpr float kMinScale = 0.4f;
 
-    [[nodiscard]] static constexpr bool isSound(std::string_view soundName) noexcept
-    {
-        return soundName == cs2::kBombPlantSoundPath;
+    [[nodiscard]] static constexpr bool is_sound(std::string_view sound_name) noexcept {
+        return sound_name == cs2::kBombPlantSoundPath;
     }
 };
