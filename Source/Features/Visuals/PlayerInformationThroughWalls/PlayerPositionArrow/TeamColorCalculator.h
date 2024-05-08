3119671add0f2c2fe54dabe14849429191bec806
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:48 +0000

    improve

--- Source/Features/Visuals/PlayerInformationThroughWalls/PlayerPositionArrow/TeamColorCalculator.h
+++ Source/Features/Visuals/PlayerInformationThroughWalls/PlayerPositionArrow/TeamColorCalculator.h
@@ -5,15 +5,19 @@
 #include <FeatureHelpers/TeamNumber.h>
 
 struct TeamColorCalculator {
-    [[nodiscard]] cs2::Color getTeamColor() const noexcept
-    {
+    TeamColorCalculator(TeamNumber team_number) : teamNumber(team_number) {}
+
+    [[nodiscard]] cs2::Color getTeamColor() const noexcept {
         switch (teamNumber) {
-            using enum TeamNumber;
-            case TT: return cs2::kColorTeamTT;
-            case CT: return cs2::kColorTeamCT;
+            case TeamNumber::TT: return cs2::kColorTeamTT;
+            case TeamNumber::CT: return cs2::kColorTeamCT;
             default: return cs2::kColorWhite;
         }
     }
 
     TeamNumber teamNumber;
 };
+
+// Example usage:
+// TeamColorCalculator calculator(TeamNumber::TT);
+// cs2::Color team_color = calculator.getTeamColor();
