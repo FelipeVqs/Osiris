b7b5d4e97ffdcac6b98c1ea623cc636a06d83091
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:37 +0000

    improve

--- Source/Features/Visuals/PlayerInformationThroughWalls/PlayerPositionArrow/PlayerColorCalculator.h
+++ Source/Features/Visuals/PlayerInformationThroughWalls/PlayerPositionArrow/PlayerColorCalculator.h
@@ -1,24 +1,38 @@
 #pragma once
 
 #include <utility>
+#include <vector>
 
 #include <CS2/Classes/Color.h>
 #include <CS2/Classes/Entities/CCSPlayerController.h>
 #include <CS2/Constants/ColorConstants.h>
 
+/**
+ * A template class for calculating the color of a player based on their teammate color index.
+ * @tparam PlayerColorIndexAccessor A type that can access the teammate color index of a player.
+ */
 template <typename PlayerColorIndexAccessor>
 struct PlayerColorCalculator {
     PlayerColorIndexAccessor playerColorIndexAccessor;
 
-    [[nodiscard]] bool getPlayerColor(cs2::Color* color) const noexcept
-    {
+    /**
+     * Gets the color of the player with the given teammate color index.
+     * @param color A pointer to a cs2::Color object to store the player's color.
+     * @return True if the player's color was successfully retrieved, false otherwise.
+     */
+    [[nodiscard]] bool getPlayerColor(cs2::Color* color) const {
         return getPlayerColor(playerColorIndexAccessor.getPlayerColorIndex(), color);
     }
 
 private:
-    [[nodiscard]] bool getPlayerColor(cs2::CCSPlayerController::m_iCompTeammateColor playerColorIndex, cs2::Color* color) const noexcept
-    {
-        if (playerColorIndex >= 0 && std::cmp_less(playerColorIndex, cs2::kPlayerColors.size())) {
+    /**
+     * Gets the color of the player with the given teammate color index.
+     * @param playerColorIndex The teammate color index of the player.
+     * @param color A pointer to a cs2::Color object to store the player's color.
+     * @return True if the player's color was successfully retrieved, false otherwise.
+     */
+    [[nodiscard]] bool getPlayerColor(cs2::CCSPlayerController::m_iCompTeammateColor playerColorIndex, cs2::Color* color) const {
+        if (playerColorIndex >= 0 && playerColorIndex < cs2::kPlayerColors.size()) {
             *color = cs2::kPlayerColors[playerColorIndex];
             return true;
         }
@@ -26,5 +40,9 @@ private:
     }
 };
 
+/**
+ * A constructor template alias for creating PlayerColorCalculator objects.
+ * @tparam PlayerColorIndexAccessor A type that can access the teammate color index of a player.
+ */
 template <typename PlayerColorIndexAccessor>
 PlayerColorCalculator(PlayerColorIndexAccessor) -> PlayerColorCalculator<PlayerColorIndexAccessor>;
