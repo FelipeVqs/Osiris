fa123a3445031beb6b8fdc0c1a961b5ed49412a2
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:32 +0000

    improve

--- Source/Utils/MemorySection.h
+++ Source/Utils/MemorySection.h
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <span>
 
+static_assert(sizeof(std::uintptr_t) >= sizeof(std::byte*), "std::uintptr_t is not large enough to hold a pointer to std::byte");
+
 struct MemorySection {
     MemorySection() = default;
 
@@ -13,22 +15,22 @@ struct MemorySection {
     {
     }
 
-    [[nodiscard]] std::span<const std::byte> raw() const noexcept
+    [[nodiscard]] const std::span<const std::byte> raw() const noexcept
     {
         return {reinterpret_cast<const std::byte*>(base), size};
     }
 
-    [[nodiscard]] bool contains(std::uintptr_t address, std::size_t objectSize) const noexcept
+    [[nodiscard]] constexpr bool contains(std::uintptr_t address, std::size_t objectSize) const noexcept requires(address <= std::numeric_limits<std::uintptr_t>::max())
     {
         return address >= base && size >= objectSize && (address - base) <= size - objectSize;
     }
 
-    [[nodiscard]] bool contains(std::uintptr_t address) const noexcept
+    [[nodiscard]] constexpr bool contains(std::uintptr_t address) const noexcept requires(address <= std::numeric_limits<std::uintptr_t>::max())
     {
         return address >= base && address - base < size;
     }
 
-    [[nodiscard]] std::size_t offsetOf(std::uintptr_t address) const noexcept
+    [[nodiscard]] std::size_t offsetOf(std::uintptr_t address) const noexcept requires(address <= std::numeric_limits<std::uintptr_t>::max())
     {
         assert(contains(address));
         return address - base;
