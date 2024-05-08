c33901630f24ccf29672d446432895bed6008d98
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:29 +0000

    improve

--- Source/GameClasses/OffsetTypes/WeaponVDataOffset.h
+++ Source/GameClasses/OffsetTypes/WeaponVDataOffset.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <cstdint>
-
 #include <CS2/Classes/CCSWeaponBaseVData.h>
 #include <Utils/FieldOffset.h>
 
+// A template for defining a type that represents the offset of a field in a CCSWeaponBaseVData object.
 template <typename FieldType, typename OffsetType>
-using WeaponVDataOffset = FieldOffset<cs2::CCSWeaponBaseVData, FieldType, OffsetType>;
+using WeaponVDataOffset = FieldOffset<CS2::CCSWeaponBaseVData, FieldType, OffsetType>;
 
-using OffsetToWeaponName = WeaponVDataOffset<cs2::CCSWeaponBaseVData::m_szName, std::int32_t>;
+// The offset of the m_szName field in a CCSWeaponBaseVData object.
+using OffsetToWeaponName = WeaponVDataOffset<CS2::CCSWeaponBaseVData::m_szName, std::int32_t>;
