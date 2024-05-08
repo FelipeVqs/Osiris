eca1d2f51d78ea08a9c91804b6db2b3cbd7eadbd
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:42 +0000

    improve

--- Source/Utils/CharUtils.h
+++ Source/Utils/CharUtils.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include <cctype>
+#include <functional>
+
 namespace utils
 {
 
 [[nodiscard]] constexpr wchar_t toUpper(wchar_t c) noexcept
 {
-    if (c >= L'a' && c <= L'z')
-        return c - L'a' + L'A';
-    return c;
+    // Use std::toupper to handle invalid input and avoid branching
+    return std::toupper(c);
 }
 
 }
