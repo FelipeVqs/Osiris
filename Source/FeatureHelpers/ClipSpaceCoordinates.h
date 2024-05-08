686cf4a8a764d193fc283c3b1528a36bea4afb88
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:21 +0000

    improve

--- Source/FeatureHelpers/ClipSpaceCoordinates.h
+++ Source/FeatureHelpers/ClipSpaceCoordinates.h
@@ -1,18 +1,21 @@
 #pragma once
 
 #include <cassert>
+#include <cmath> // for std::abs
 
 #include "NormalizedDeviceCoordinates.h"
 
 struct ClipSpaceCoordinates {
     [[nodiscard]] bool onScreen() const noexcept
     {
-        return w >= 0.001f;
+        return w >= 0.001f && std::abs(w) >= std::numeric_limits<float>::epsilon();
     }
 
     [[nodiscard]] NormalizedDeviceCoordinates toNormalizedDeviceCoordinates() const noexcept
     {
-        assert(onScreen());
+        if (!onScreen()) {
+            return NormalizedDeviceCoordinates{ 0.f, 0.f };
+        }
         const auto inverseW = 1.0f / w;
         return NormalizedDeviceCoordinates{ x * inverseW, y * inverseW };
     }
