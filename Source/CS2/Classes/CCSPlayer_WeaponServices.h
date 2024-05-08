a5f4a21e9c1841856e7ff3980a8fbdf538b75add
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:18 +0000

    improve

--- Source/CS2/Classes/CCSPlayer_WeaponServices.h
+++ Source/CS2/Classes/CCSPlayer_WeaponServices.h
@@ -1,12 +1,17 @@
-#pragma once
+#ifndef CS2_CCS_PLAYER_WEAPON_SERVICES_H
+#define CS2_CCS_PLAYER_WEAPON_SERVICES_H
 
-#include "EntitySystem/CEntityHandle.h"
+#include <EntitySystem/IEntityHandle.h> // use the interface instead of the concrete implementation
 
 namespace cs2
 {
 
-struct CCSPlayer_WeaponServices {
-    using m_hActiveWeapon = CEntityHandle;
+struct CCSPlayerWeaponServices {
+    using ActiveWeaponHandle = const IEntityHandle<class CWeaponEntity>;
+
+    ActiveWeaponHandle m_activeWeapon;
 };
 
-}
+} // namespace cs2
+
+#endif // CS2_CCS_PLAYER_WEAPON_SERVICES_H
