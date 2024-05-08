1b0022be415e6593850e199b1f211386e88c8182
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:33 +0000

    improve

--- Source/GameClasses/Implementation/GameSceneNodeImpl.h
+++ Source/GameClasses/Implementation/GameSceneNodeImpl.h
@@ -2,11 +2,16 @@
 
 #include <MemoryPatterns/GameSceneNodePatterns.h>
 
+// Represents an implementation of a GameSceneNode.
 struct GameSceneNodeImpl {
+    // Constructs a GameSceneNodeImpl with the given patterns.
     explicit GameSceneNodeImpl(const GameSceneNodePatterns& gameSceneNodePatterns) noexcept
-        : offsetToAbsOrigin{gameSceneNodePatterns.offsetToAbsOrigin()}
-    {
-    }
+        : offsetToAbsOrigin{gameSceneNodePatterns.offsetToAbsOrigin()} {}
 
-    OffsetToAbsOrigin offsetToAbsOrigin;
+    // Constructs a default GameSceneNodeImpl.
+    GameSceneNodeImpl() noexcept = default;
+
+    // The offset to the absolute origin of the GameSceneNode.
+    const OffsetToAbsOrigin offsetToAbsOrigin;
 };
+
