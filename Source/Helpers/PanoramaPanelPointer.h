69d5eaecf0d5dcd44b2c596c145331296935b18c
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:41 +0000

    improve

--- Source/Helpers/PanoramaPanelPointer.h
+++ Source/Helpers/PanoramaPanelPointer.h
@@ -1,24 +1,52 @@
 #pragma once
 
 #include <cassert>
-
-#include <CS2/Classes/Panorama.h>
+#include <cs2/Classes/Panorama.h>
 #include <GameClasses/PanoramaUiEngine.h>
 #include <GameClasses/PanoramaUiPanel.h>
 
 struct PanoramaPanelPointer {
-    explicit(false) PanoramaPanelPointer(cs2::CUIPanel* panel) noexcept
-        : handle{ PanoramaUiEngine::getPanelHandle(panel) }
+    explicit PanoramaPanelPointer(cs2::CUIPanel* panel, bool success = true) noexcept
+        : handle{ success ? PanoramaUiEngine::getPanelHandle(panel) : cs2::PanelHandle() }
     {
         assert(!panel || handle.isValid());
     }
 
-    explicit(false) PanoramaPanelPointer(PanoramaUiPanel panel) noexcept
+    explicit PanoramaPanelPointer(PanoramaUiPanel panel) noexcept
         : PanoramaPanelPointer{ static_cast<cs2::CUIPanel*>(panel) }
     {
     }
 
-    PanoramaPanelPointer() = default;
+    PanoramaPanelPointer(const PanoramaPanelPointer& other) noexcept
+        : handle{ other.handle }
+    {
+    }
+
+    PanoramaPanelPointer& operator=(const PanoramaPanelPointer& other) noexcept
+    {
+        handle = other.handle;
+        return *this;
+    }
+
+    PanoramaPanelPointer(PanoramaPanelPointer&& other) noexcept
+        : handle{ std::move(other.handle) }
+    {
+    }
+
+    PanoramaPanelPointer& operator=(PanoramaPanelPointer&& other) noexcept
+    {
+        handle = std::move(other.handle);
+        return *this;
+    }
+
+    ~PanoramaPanelPointer() noexcept
+    {
+    }
+
+    void swap(PanoramaPanelPointer& other) noexcept
+    {
+        handle.swap(other.handle);
+    }
 
     [[nodiscard]] cs2::PanelHandle getHandle() const noexcept
     {
@@ -28,6 +56,26 @@ struct PanoramaPanelPointer {
     [[nodiscard]] PanoramaUiPanel get() const noexcept
     {
         return PanoramaUiPanel{ PanoramaUiEngine::getPanelPointer(handle) };
+    }
+
+    [[nodiscard]] explicit operator bool() const noexcept
+    {
+        return handle.isValid();
+    }
+
+    friend bool operator==(const PanoramaPanelPointer& lhs, const PanoramaPanelPointer& rhs) noexcept
+    {
+        return lhs.handle == rhs.handle;
+    }
+
+    friend bool operator!=(const PanoramaPanelPointer& lhs, const PanoramaPanelPointer& rhs) noexcept
+    {
+        return lhs.handle != rhs.handle;
+    }
+
+    void reset() noexcept
+    {
+        handle = cs2::PanelHandle();
     }
 
 private:
