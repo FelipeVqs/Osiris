d38dccb34870cafb7c20cbc32c698658bfc89021
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:50 +0000

    improve

--- Source/Features/Sound/Details/FootstepSound.h
+++ Source/Features/Sound/Details/FootstepSound.h
@@ -6,15 +6,24 @@
 
 #include <CS2/Constants/SoundNames.h>
 
+namespace cs2 {
+using StringView = std::string_view;
+}
+
 struct FootstepSound {
     static constexpr auto kFadeAwayStart = 1.6f;
     static constexpr auto kFadeAwayDuration = 0.4f;
     static constexpr auto kMinScale = 0.3f;
 
-    [[nodiscard]] static constexpr bool isSound(std::string_view soundName) noexcept
+    static_assert(kFadeAwayStart > kFadeAwayDuration, "kFadeAwayStart must be greater than kFadeAwayDuration");
+
+    [[nodiscard]] static constexpr bool isSound(StringView soundName) noexcept
     {
-        if (soundName.starts_with(cs2::kPlayerFootstepSoundsPath)) {
-            return !std::string_view{ soundName.data() + cs2::kPlayerFootstepSoundsPath.length(), soundName.length() - cs2::kPlayerFootstepSoundsPath.length() }.starts_with(cs2::kPlayerSuitSoundPrefix);
+        constexpr auto footstepSoundsPath = cs2::kPlayerFootstepSoundsPath;
+        constexpr auto suitSoundsPrefix = cs2::kPlayerSuitSoundPrefix;
+
+        if (soundName.starts_with(footstepSoundsPath)) {
+            return !soundName.substr(footstepSoundsPath.length()).starts_with(suitSoundsPrefix);
         }
         return false;
     }
