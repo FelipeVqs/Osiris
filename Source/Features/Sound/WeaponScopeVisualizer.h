37a48ed432acace2d94d4fb7fa8af40d5648f88c
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:24:36 +0000

    improve

--- Source/Features/Sound/WeaponScopeVisualizer.h
+++ Source/Features/Sound/WeaponScopeVisualizer.h
@@ -1,17 +1,22 @@
 #pragma once
 
 #include <CS2/Classes/Panorama.h>
-
 #include "Details/SoundVisualizationFeature.h"
 #include "Details/SoundVisualizationPanelProperties.h"
 
 struct WeaponScopePanels {
-    [[nodiscard]] static SoundVisualizationPanelProperties soundVisualizationPanelProperties() noexcept
-    {
-        return SoundVisualizationPanelProperties{
-            .svgImagePath = "s2r://panorama/images/icons/ui/chatwheel_sniperspotted.svg",
-            .position = SoundVisualizationPosition::AtOrigin};
-    }
+    WeaponScopePanels(const std::string& svgImagePath, SoundVisualizationPosition position)
+        : panelProperties{svgImagePath, position} {}
+
+    SoundVisualizationPanelProperties panelProperties;
 };
+
+using WeaponScopeSound = void(); // not sure what this type should be
+
+inline SoundVisualizationPanelProperties WeaponScopePanels::soundVisualizationPanelProperties() noexcept {
+    return SoundVisualizationPanelProperties{
+        .svgImagePath = "s2r://panorama/images/icons/ui/chatwheel_sniperspotted.svg",
+        .position = SoundVisualizationPosition::AtOrigin};
+}
 
 using WeaponScopeVisualizer = SoundVisualizationFeature<WeaponScopePanels, WeaponScopeSound>;
