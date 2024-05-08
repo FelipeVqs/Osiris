af2736643773a60a7fd45e49e2239bce062450e6
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:35 +0000

    improve

--- Source/MemoryPatterns/Linux/LinuxPatterns.h
+++ Source/MemoryPatterns/Linux/LinuxPatterns.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#if defined(LINUX) || defined(__LINUX__) || defined(__linux__)
 #include "ClientPatternsLinux.h"
 #include "ConVarPatternsLinux.h"
 #include "CvarPatternsLinux.h"
@@ -23,3 +24,9 @@
 #include "TopLevelWindowPatternsLinux.h"
 #include "WeaponServicesPatternsLinux.h"
 #include "WeaponVDataPatternsLinux.h"
+#elif defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(WIN64)
+// Include Windows-specific headers here
+#endif
+
+
+#include "PlatformHeaders.h"
