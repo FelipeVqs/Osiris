8e66272ab5d0d7e9ef712cfea04e3babfbf3076d
Vasques <29583401+FelipeVqs@users.noreply.github.com>
2024-05-08 12:23:42 +0000

    improve

--- Source/GameClasses/Implementation/GameClassImplementations.h
+++ Source/GameClasses/Implementation/GameClassImplementations.h
@@ -1,28 +1,52 @@
 #pragma once
 
+#include <memory> // for std::unique_ptr
+#include <string> // for std::string
+
+// Includes for GameClassImplementations
 #include "ConVarImpl.h"
 #include "CvarImpl.h"
 #include "EntityImpl.h"
 #include "EntitySystemImpl.h"
-#include "FileNameSymbolTableImpl.h"
-#include "FileSystemImpl.h"
 #include "GameRulesImpl.h"
 #include "GameSceneNodeImpl.h"
 #include "MemAllocImpl.h"
-#include "PanelImpl.h"
-#include "PanelStyleImpl.h"
-#include "PanoramaImagePanelImpl.h"
-#include "PanoramaLabelImpl.h"
-#include "PanoramaUiEngineImpl.h"
-#include "PanoramaUiPanelImpl.h"
-#include "PlantedC4Impl.h"
 #include "PlayerControllerImpl.h"
 #include "PlayerPawnImpl.h"
-#include "TopLevelWindowImpl.h"
 #include "WeaponServicesImpl.h"
 #include "WeaponVDataImpl.h"
 
-#include <Platform/VmtFinder.h>
+// Includes for PatternFinders
+#include "Pattern/VmtFinder.h"
+#include "Patterns/ConVarPatterns.h"
+#include "Patterns/CvarPatterns.h"
+#include "Patterns/EntityPatterns.h"
+#include "Patterns/EntitySystemPatterns.h"
+#include "Patterns/FileSystemPatterns.h"
+#include "Patterns/GameRulesPatterns.h"
+#include "Patterns/GameSceneNodePatterns.h"
+#include "Patterns/MemAllocPatterns.h"
+#include "Patterns/PanelPatterns.h"
+#include "Patterns/PanelStylePatterns.h"
+#include "Patterns/PanoramaImagePanelPatterns.h"
+#include "Patterns/PanoramaLabelPatterns.h"
+#include "Patterns/PanoramaUiEnginePatterns.h"
+#include "Patterns/PanoramaUiPanelPatterns.h"
+#include "Patterns/PlantedC4Patterns.h"
+#include "Patterns/PlayerControllerPatterns.h"
+#include "Patterns/PlayerPawnPatterns.h"
+#include "Patterns/TopLevelWindowPatterns.h"
+#include "Patterns/WeaponServicesPatterns.h"
+#include "Patterns/WeaponVDataPatterns.h"
+
+// Includes for Tier0
+#include "Tier0/tier0.h"
+
+// Includes for Panorama
+#include "panorama_ui/panorama_ui.h"
+
+// Includes for FileSystem
+#include "filesystem_stdio.h"
 
 struct GameClassImplementations {
     GameClassImplementations(const PatternFinder<PatternNotFoundLogger>& clientPatternFinder,
@@ -30,27 +54,27 @@ struct GameClassImplementations {
                              const PatternFinder<PatternNotFoundLogger>& tier0PatternFinder,
                              const FileSystemPatterns& fileSystemPatterns,
                              Tier0Dll tier0Dll) noexcept
-        : conVar{ConVarPatterns{tier0PatternFinder}}
-        , cvar{CvarPatterns{clientPatternFinder, tier0PatternFinder}}
-        , entity{EntityPatterns{clientPatternFinder}}
-        , entitySystem{EntitySystemPatterns{clientPatternFinder}}
-        , fileNameSymbolTable{tier0Dll}
-        , fileSystem{fileSystemPatterns}
-        , gameRules{GameRulesPatterns{clientPatternFinder}}
-        , gameSceneNode{GameSceneNodePatterns{clientPatternFinder}}
-        , memAlloc{tier0Dll, MemAllocPatterns{clientPatternFinder}}
-        , panel{PanelPatterns{clientPatternFinder}}
-        , panelStyle{PanelStylePatterns{panoramaPatternFinder}}
-        , imagePanel{PanoramaImagePanelPatterns{clientPatternFinder}}
-        , panoramaLabel{PanoramaLabelPatterns{clientPatternFinder}}
-        , uiEngine{ClientPatterns{clientPatternFinder}, PanoramaUiEnginePatterns{panoramaPatternFinder}}
-        , panoramaUiPanelOffsets{PanoramaUiPanelPatterns{clientPatternFinder, panoramaPatternFinder}}
-        , plantedC4{PlantedC4Patterns{clientPatternFinder}}
-        , playerController{PlayerControllerPatterns{clientPatternFinder}}
-        , playerPawn{PlayerPawnPatterns{clientPatternFinder}}
-        , topLevelWindow{TopLevelWindowPatterns{panoramaPatternFinder}}
-        , weaponServices{WeaponServicesPatterns{clientPatternFinder}}
-        , weaponVData{WeaponVDataPatterns{clientPatternFinder}}
+        : conVar(tier0PatternFinder)
+        , cvar(clientPatternFinder, tier0PatternFinder)
+        , entity(clientPatternFinder)
+        , entitySystem(clientPatternFinder)
+        , fileNameSymbolTable(std::move(tier0Dll))
+        , fileSystem(fileSystemPatterns)
+        , gameRules(clientPatternFinder)
+        , gameSceneNode(clientPatternFinder)
+        , memAlloc(std::move(tier0Dll), clientPatternFinder)
+        , panel(clientPatternFinder)
+        , panelStyle(panoramaPatternFinder)
+        , imagePanel(clientPatternFinder)
+        , panoramaLabel(clientPatternFinder)
+        , uiEngine(clientPatternFinder, panoramaPatternFinder)
+        , panoramaUiPanelOffsets(clientPatternFinder, panoramaPatternFinder)
+        , plantedC4(clientPatternFinder)
+        , playerController(clientPatternFinder)
+        , playerPawn(clientPatternFinder)
+        , topLevelWindow(panoramaPatternFinder)
+        , weaponServices(clientPatternFinder)
+        , weaponVData(clientPatternFinder)
     {
     }
 
